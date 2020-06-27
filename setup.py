e2d2a98df64c11d0e6b041128c3719b4fedbba24
root-11 <bjorn.h.madsen@gmail.com>
2020-06-27 15:01:34 +0100

    added package and setup for uploading to pypi

---
+++ setup.py
@@ -0,0 +1,50 @@
+"""
+tablite
+"""
+build_tag = "b2b4e06c02f986488f6d51a2e393e3bad46680648aedd19a292f9fca62e8860a"
+from setuptools import setup
+from pathlib import Path
+
+
+folder = Path(__file__).parent
+file = "README.md"
+readme = folder / file
+assert isinstance(readme, Path)
+assert readme.exists(), readme
+with open(str(readme), encoding='utf-8') as f:
+    long_description = f.read()
+
+keywords = list({
+    'tables'
+})
+
+keywords.sort(key=lambda x: x.lower())
+
+
+setup(
+    name="tablite",
+    version="2020.5.6.39102",
+    url="https://github.com/root-11/tablite",
+    license="MIT",
+    author="Bjorn Madsen",
+    author_email="bjorn.madsen@operationsresearchgroup.com",
+    description="A table crunching library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=keywords,
+    packages=["table"],
+    include_package_data=True,
+    data_files=[(".", ["LICENSE", "README.md"])],
+    platforms="any",
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
+
+
