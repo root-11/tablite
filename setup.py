cf761df9c7b9e75dcd473eb3313d4bace9a9c5de
root-11 <bjorn.h.madsen@gmail.com>
2021-02-18 15:06:34 +0000

    removed int(float(x)) as large x can cause loss of precision.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 tablite
 """
-build_tag = "789be7d6fe5c13d737e7b6343a5f44694aa29a7bb8aa1eeeea08c3cd9ebb07a8"
+build_tag = "b847975e9ecb411ec8644e10154e639a99d9806dd3f620b41272fd77291ed44e"
 from setuptools import setup
 from pathlib import Path
 
@@ -31,7 +31,7 @@ with open('requirements.txt', 'r') as fi:
 
 setup(
     name="tablite",
-    version="2021.2.15.44662",
+    version="2021.2.18.54360",
     url="https://github.com/root-11/tablite",
     license="MIT",
     author="Bjorn Madsen",
