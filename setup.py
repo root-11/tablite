2b4d06e3f62877229f9af0276cecaae30110556f
root-11 <bjorn.h.madsen@gmail.com>
2022-01-24 15:25:22 +0000

    Minor typo fixed.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 tablite
 """
-build_tag = "3be2cb3b14535932bd7f554f8fe466ade68b44af45ed36d91a39ea5cca32ffa8"
+build_tag = "0812460af5cd45a555e81fc01be1710cb1b839ed3a3cc6354b5ad6cc212d95aa"
 from setuptools import setup
 from pathlib import Path
 
@@ -31,12 +31,12 @@ with open('requirements.txt', 'r') as fi:
 
 setup(
     name="tablite",
-    version="2021.5.21.47274",
+    version="2021.11.5.66041",
     url="https://github.com/root-11/tablite",
     license="MIT",
     author="Bjorn Madsen",
     author_email="bjorn.madsen@operationsresearchgroup.com",
-    description="A tablite crunching library",
+    description="A table crunching library",
     long_description=long_description,
     long_description_content_type='text/markdown',
     keywords=keywords,
