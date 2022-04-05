841c991dce588ceb6a6a0004db16e9ae6ed85d5b
root-11 <bjorn.h.madsen@gmail.com>
2022-04-05 13:27:30 +0100

    Added ODS and XLSX readers.

--- tests/test_multi_proc.py
+++ tests/test_multi_proc.py
@@ -51,7 +51,7 @@ class TaskManager(object):
         elif isinstance(tasks, (list,tuple)):
             self._tasks.extend([i for i in tasks])
         else:
-            raise TypeError(f"expected instance of Task, got {type(task)}")
+            raise TypeError(f"expected instance of Task, got {type(tasks)}")
     
     def __enter__(self):
         self.start()
@@ -2340,80 +2340,80 @@ def sha256sum(path, column_name):
     raise TimeoutError("Couldn't connect to OS.")
 
 
-def excel_reader(path, has_headers=True, sheet_names=None, **kwargs):
+def excel_reader(path, has_headers=True, sheet_name=None, **kwargs):
     """  returns Table(s) from excel path """
     if not isinstance(path, pathlib.Path):
         raise ValueError(f"expected pathlib.Path, got {type(path)}")
     book = pyexcel.get_book(file_name=str(path))
 
+    if sheet_name is None:  # help the user.
+        raise ValueError(f"No sheet_name declared: \navailable sheets:\n{[s.name for s in book]}")
+    elif sheet_name not in {s.name for s in book}:
+        raise ValueError(f"sheet not found: {sheet_name}")
+
     # import all sheets or a subset
-    sheets = [s for s in book if sheet_names is None or s.name in sheet_names]
-
-    for sheet in sheets:
-        if len(sheet) == 0:
+    for sheet in book:
+        if sheet.name != sheet_name:
             continue
+        else:
+            break
+    
+    t = Table()
+    for idx, column in enumerate(sheet.columns(), 1):
+        if has_headers:
+            header, start_row_pos = str(column[0]), 1
+        else:
+            header, start_row_pos = f"_{idx}", 0
 
-        t = Table()
-        t.metadata['sheet_name'] = sheet.name
-        t.metadata['filename'] = path.name
-        for idx, column in enumerate(sheet.columns(), 1):
-            if has_headers:
-                header, start_row_pos = str(column[0]), 1
-            else:
-                header, start_row_pos = f"_{idx}", 0
+        dtypes = {type(v) for v in column[start_row_pos:]}
+        dtypes.discard(None)
 
-            dtypes = {type(v) for v in column[start_row_pos:]}
-            dtypes.discard(None)
+        if dtypes == {int, float}:
+            dtypes.remove(int)
 
-            if dtypes == {int, float}:
-                dtypes.remove(int)
-
-            if len(dtypes) == 1:
-                dtype = dtypes.pop()
-                data = [dtype(v) if not isinstance(v, dtype) else v for v in column[start_row_pos:]]
-            else:
-                dtype, data = str, [str(v) for v in column[start_row_pos:]]
-            t.add_column(header, data)
-        yield t
+        if len(dtypes) == 1:
+            dtype = dtypes.pop()
+            data = [dtype(v) if not isinstance(v, dtype) else v for v in column[start_row_pos:]]
+        else:
+            dtype, data = str, [str(v) for v in column[start_row_pos:]]
+        t.add_column(header, data)
+    return t
 
 
-def ods_reader(path, has_headers=True, **kwargs):
+def ods_reader(path, has_headers=True, sheet_name=None, **kwargs):
     """  returns Table from .ODS """
     if not isinstance(path, pathlib.Path):
         raise ValueError(f"expected pathlib.Path, got {type(path)}")
     sheets = pyexcel.get_book_dict(file_name=str(path))
 
-    for sheet_name, data in sheets.items():
-        if all((row == [] for row in data)):  # no data.
-            continue
-        for i in range(len(data)):  # remove empty lines at the end of the data.
-            if "" == "".join(str(i) for i in data[-1]):
-                data = data[:-1]
-            else:
-                break
+    if sheet_name is None or sheet_name not in sheets:
+        raise ValueError(f"No sheet_name declared: \navailable sheets:\n{[s.name for s in sheets]}")
+            
+    data = sheets[sheet_name]
+    for _ in range(len(data)):  # remove empty lines at the end of the data.
+        if "" == "".join(str(i) for i in data[-1]):
+            data = data[:-1]
+        else:
+            break
 
-        table = Table(use_disk=kwargs.get('use_disk', None))
-        table.metadata['filename'] = path.name
-        table.metadata['sheet_name'] = sheet_name
+    t = Table()
+    for ix, value in enumerate(data[0]):
+        if has_headers:
+            header, start_row_pos = str(value), 1
+        else:
+            header, start_row_pos = f"_{ix + 1}", 0
 
-        for ix, value in enumerate(data[0]):
-            if has_headers:
-                header, start_row_pos = str(value), 1
-            else:
-                header, start_row_pos = f"_{ix + 1}", 0
-
-            dtypes = set(type(row[ix]) for row in data[start_row_pos:] if len(row) > ix)
-            allow_empty = None in dtypes
-            dtypes.discard(None)
-            if len(dtypes) == 1:
-                dtype = dtypes.pop()
-            elif dtypes == {float, int}:
-                dtype = float
-            else:
-                dtype = str
-            values = [dtype(row[ix]) for row in data[start_row_pos:] if len(row) > ix]
-            table.add_column(header, dtype, allow_empty, data=values)
-        yield table
+        dtypes = set(type(row[ix]) for row in data[start_row_pos:] if len(row) > ix)
+        dtypes.discard(None)
+        if len(dtypes) == 1:
+            dtype = dtypes.pop()
+        elif dtypes == {float, int}:
+            dtype = float
+        else:
+            dtype = str
+        values = [dtype(row[ix]) for row in data[start_row_pos:] if len(row) > ix]
+        t.add_column(header, data=values)
+    return t
 
 
 # TESTS -----------------
@@ -2588,6 +2588,7 @@ def test_slicing():
     a_preview.show(format='ascii')
     
 
+# PARALLEL TASK FUNCTION
 def mem_test_job(shm_name, dtype, shape, index, value):
     """
     function for TaskManager for test_multiprocessing
@@ -2767,8 +2768,8 @@ def test_file_importer_multiproc():
 GLOBAL_CLEANUP = False
 
 if __name__ == "__main__":
-    # test_basics2()
-    test_file_importer_multiproc()
+    test_basics2()
+    # test_file_importer_multiproc()
 
     # for k,v in {k:v for k,v in sorted(globals().items()) if k.startswith('test') and callable(v)}.items():
     #     print(20 * "-" + k + "-" * 20)
