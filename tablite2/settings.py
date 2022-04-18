fedaa6c527256607f2db85f458fe9abaf3f0b95c
root-11 <bjorn.h.madsen@gmail.com>
2022-04-18 14:47:38 +0100

    added  outline for column and page.

--- tablite2/settings.py
+++ tablite2/settings.py
@@ -4,3 +4,5 @@ import os
 HDF5_IMPORT_ROOT = "__h5_import"  # the hdf5 base name for imports. f.x. f['/__h5_import/column A']
 MEMORY_MANAGER_CACHE_DIR = os.getcwd()
 MEMORY_MANAGER_CACHE_FILE = "tablite_cache.hdf5"
+MEMORY_MANAGER_PAGE_SIZE = 1_000_000
+MEMORY_USAGE_CEILING = 0.9 # 90%
