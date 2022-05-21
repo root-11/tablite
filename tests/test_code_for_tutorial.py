1ab885b0d49be623aacfd11ae55595304159923e
root-11 <bjorn.h.madsen@gmail.com>
2022-05-21 20:58:10 +0100

    moved all tests for the tutorials.

--- tests/test_code_for_tutorial.py
+++ tests/test_code_for_tutorial.py
@@ -179,6 +179,368 @@ def test_basic_table():
     table5_from_json = Table.from_json(table5_json)
     assert table5 == table5_from_json
 
+def test_join():
+    numbers = Table(use_disk=True)
+    numbers.add_column('number', int, allow_empty=True, data=[1, 2, 3, 4, None])
+    numbers.add_column('colour', str, data=['black', 'blue', 'white', 'white', 'blue'])
+
+    letters = Table(use_disk=True)
+    letters.add_column('letter', str, allow_empty=True, data=['a', 'b', 'c', 'd', None])
+    letters.add_column('color', str, data=['blue', 'white', 'orange', 'white', 'blue'])
+
+    # left join
+    # SELECT number, letter FROM numbers LEFT JOIN letters ON numbers.colour == letters.color
+    left_join = numbers.left_join(letters, left_keys=['colour'], right_keys=['color'], left_columns=['number'], right_columns=['letter'])
+    left_join.show()
+    # +======+======+
+    # |number|letter|
+    # | int  | str  |
+    # | True | True |
+    # +------+------+
+    # |     1|None  |
+    # |     2|a     |
+    # |     2|None  |
+    # |     3|b     |
+    # |     3|d     |
+    # |     4|b     |
+    # |     4|d     |
+    # |None  |a     |
+    # |None  |None  |
+    # +======+======+
+    assert [i for i in left_join['number']] == [1, 2, 2, 3, 3, 4, 4, None, None]
+    assert [i for i in left_join['letter']] == [None, 'a', None, 'b', 'd', 'b', 'd', 'a', None]
+
+    # inner join
+    # SELECT number, letter FROM numbers JOIN letters ON numbers.colour == letters.color
+    inner_join = numbers.inner_join(letters, left_keys=['colour'], right_keys=['color'], left_columns=['number'], right_columns=['letter'])
+    inner_join.show()
+    # +======+======+
+    # |number|letter|
+    # | int  | str  |
+    # | True | True |
+    # +------+------+
+    # |     2|a     |
+    # |     2|None  |
+    # |None  |a     |
+    # |None  |None  |
+    # |     3|b     |
+    # |     3|d     |
+    # |     4|b     |
+    # |     4|d     |
+    # +======+======+
+    assert [i for i in inner_join['number']] == [2, 2, None, None, 3, 3, 4, 4]
+    assert [i for i in inner_join['letter']] == ['a', None, 'a', None, 'b', 'd', 'b', 'd']
+
+    # outer join
+    # SELECT number, letter FROM numbers OUTER JOIN letters ON numbers.colour == letters.color
+    outer_join = numbers.outer_join(letters, left_keys=['colour'], right_keys=['color'], left_columns=['number'], right_columns=['letter'])
+    outer_join.show()
+    # +======+======+
+    # |number|letter|
+    # | int  | str  |
+    # | True | True |
+    # +------+------+
+    # |     1|None  |
+    # |     2|a     |
+    # |     2|None  |
+    # |     3|b     |
+    # |     3|d     |
+    # |     4|b     |
+    # |     4|d     |
+    # |None  |a     |
+    # |None  |None  |
+    # |None  |c     |
+    # +======+======+
+    assert [i for i in outer_join['number']] == [1, 2, 2, 3, 3, 4, 4, None, None, None]
+    assert [i for i in outer_join['letter']] == [None, 'a', None, 'b', 'd', 'b', 'd', 'a', None, 'c']
+
+    assert left_join != inner_join
+    assert inner_join != outer_join
+    assert left_join != outer_join
+
+def test_left_join():
+    """ joining a table on itself. Wierd but possible. """
+    numbers = Table()
+    numbers.add_column('number', int, allow_empty=True, data=[1, 2, 3, 4, None])
+    numbers.add_column('colour', str, data=['black', 'blue', 'white', 'white', 'blue'])
+
+    left_join = numbers.left_join(numbers, left_keys=['colour'], right_keys=['colour'])
+
+    assert list(left_join.rows) == [(1, 'black', 1, 'black'),
+                                    (2, 'blue', 2, 'blue'),
+                                    (2, 'blue', None, 'blue'),
+                                    (3, 'white', 3, 'white'),
+                                    (3, 'white', 4, 'white'),
+                                    (4, 'white', 3, 'white'),
+                                    (4, 'white', 4, 'white'),
+                                    (None, 'blue', 2, 'blue'),
+                                    (None, 'blue', None, 'blue')]
+
+
+def test_left_join2():
+    """ joining a table on itself. Wierd but possible. """
+    numbers = Table()
+    numbers.add_column('number', int, allow_empty=True, data=[1, 2, 3, 4, None])
+    numbers.add_column('colour', str, data=['black', 'blue', 'white', 'white', 'blue'])
+
+    left_join = numbers.left_join(numbers, left_keys=['colour'], right_keys=['colour'], left_columns=['colour', 'number'], right_columns=['number', 'colour'])
+
+    assert list(left_join.rows) == [('black', 1, 1, 'black'),
+                                    ('blue', 2, 2, 'blue'),
+                                    ('blue', 2, None, 'blue'),
+                                    ('white', 3, 3, 'white'),
+                                    ('white', 3, 4, 'white'),
+                                    ('white', 4, 3, 'white'),
+                                    ('white', 4, 4, 'white'),
+                                    ('blue', None, 2, 'blue'),
+                                    ('blue', None, None, 'blue')]
+
+
+def _join_left(pairs_1, pairs_2, pairs_ans, column_1, column_2):
+    """
+    SELECT tbl1.number, tbl1.color, tbl2.number, tbl2.color
+      FROM `tbl2`
+      LEFT JOIN `tbl2`
+        ON tbl1.color = tbl2.color;
+    """
+    numbers_1 = Table()
+    numbers_1.add_column('number', int, allow_empty=True)
+    numbers_1.add_column('colour', str)
+    for row in pairs_1:
+        numbers_1.add_row(row)
+
+    numbers_2 = Table()
+    numbers_2.add_column('number', int, allow_empty=True)
+    numbers_2.add_column('colour', str)
+    for row in pairs_2:
+        numbers_2.add_row(row)
+
+    left_join = numbers_1.left_join(numbers_2, left_keys=[column_1], right_keys=[column_2], left_columns=['number','colour'], right_columns=['number','colour'])
+
+    assert len(pairs_ans) == len(left_join)
+    for a, b in zip(sorted(pairs_ans, key=lambda x: str(x)), sorted(list(left_join.rows), key=lambda x: str(x))):
+        assert a == b
+
+
+def test_same_join_1():
+    """FIDDLE: http://sqlfiddle.com/#!9/7dd756/7"""
+
+    pairs_1 = [
+        (1, 'black'),
+        (2, 'blue'),
+        (2, 'blue'),
+        (3, 'white'),
+        (3, 'white'),
+        (4, 'white'),
+        (4, 'white'),
+        (None, 'blue'),
+        (None, 'blue')
+    ]
+    pairs_2 = [
+        (1, 'black'),
+        (2, 'blue'),
+        (None, 'blue'),
+        (3, 'white'),
+        (4, 'white'),
+        (3, 'white'),
+        (4, 'white'),
+        (2, 'blue'),
+        (None, 'blue')
+    ]
+    pairs_ans = [
+        (1, 'black', 1, 'black'),
+        (2, 'blue', 2, 'blue'),
+        (2, 'blue', 2, 'blue'),
+        (2, 'blue', None, 'blue'),
+        (2, 'blue', None, 'blue'),
+        (3, 'white', 3, 'white'),
+        (3, 'white', 3, 'white'),
+        (3, 'white', 4, 'white'),
+        (3, 'white', 4, 'white'),
+        (3, 'white', 3, 'white'),
+        (3, 'white', 3, 'white'),
+        (3, 'white', 4, 'white'),
+        (3, 'white', 4, 'white'),
+        (2, 'blue', 2, 'blue'),
+        (2, 'blue', 2, 'blue'),
+        (2, 'blue', None, 'blue'),
+        (2, 'blue', None, 'blue'),
+        (None, 'blue', 2, 'blue'),
+        (None, 'blue', 2, 'blue'),
+        (None, 'blue', None, 'blue'),
+        (None, 'blue', None, 'blue'),
+        (4, 'white', 3, 'white'),
+        (4, 'white', 3, 'white'),
+        (4, 'white', 4, 'white'),
+        (4, 'white', 4, 'white'),
+        (4, 'white', 3, 'white'),
+        (4, 'white', 3, 'white'),
+        (4, 'white', 4, 'white'),
+        (4, 'white', 4, 'white'),
+        (None, 'blue', 2, 'blue'),
+        (None, 'blue', 2, 'blue'),
+        (None, 'blue', None, 'blue'),
+        (None, 'blue', None, 'blue'),
+    ]
+
+    _join_left(pairs_1, pairs_2, pairs_ans, 'colour', 'colour')
+
+
+def test_left_join_2():
+    """FIDDLE: http://sqlfiddle.com/#!9/986b2a/3"""
+
+    pairs_1 = [(1, 'black'), (2, 'blue'), (3, 'white'), (4, 'white'), (None, 'blue')]
+    pairs_ans = [
+        (1, 'black', 1, 'black'),
+        (2, 'blue', 2, 'blue'),
+        (None, 'blue', 2, 'blue'),
+        (3, 'white', 3, 'white'),
+        (4, 'white', 3, 'white'),
+        (3, 'white', 4, 'white'),
+        (4, 'white', 4, 'white'),
+        (2, 'blue', None, 'blue'),
+        (None, 'blue', None, 'blue'),
+    ]
+    _join_left(pairs_1, pairs_1, pairs_ans, 'colour', 'colour')
+
+
+def test_lookup_with_all():
+    tbl_0, tbl_1 = Table(), Table()
+    for i, tbl in enumerate([tbl_0, tbl_1]):
+        tbl.add_column("Index", int)
+        tbl.add_column("Name", str)
+        tbl.add_column("SKU", int)
+        tbl.add_row((1 - (5 * i), "Table%i" % i, 1))
+        tbl.add_row((2 - (5 * i), "Table%i" % i, 2))
+        tbl.add_row((3 - (5 * i), "Table%i" % i, 3))
+    tbl_0.add_row((4, "Table0", 42))
+    tbl_1.add_row((-1, "Table1", 13))
+    tbl_0.show()
+    tbl_1.show()
+
+    def fn_eq(a, b):
+        return a == b
+
+    def fn_neq(a, b):
+        return a != b
+
+    tbl_lookup = tbl_0.lookup(tbl_1, ("SKU", fn_eq, "SKU"), ("Index", fn_neq, "Index"))
+    assert list(tbl_lookup.rows) == [(1, 'Table0', 1, -4, 'Table1', 1),
+                                     (2, 'Table0', 2, -3, 'Table1', 2),
+                                     (3, 'Table0', 3, -2, 'Table1', 3),
+                                     (4, 'Table0', 42, None, None, None)]
+
+
+def test_lookup_with_any():
+    def fizz(b):
+        return "fizz" if b % 3 == 0 else str(b)
+
+    def buzz(b):
+        return "buzz" if b % 5 == 0 else str(b)
+
+    table1 = Table()
+    table1.add_column('A', int, data=[i for i in range(20)])
+    table1.add_column('Fizz', str, data=[fizz(i) for i in range(20)])
+    table1.add_column('Buzz', str, data=[buzz(i) for i in range(20)])
+    table1.show()
+
+    table2 = Table()
+    table2.add_column('B', str, data=[str(i) for i in range(20)])
+
+    table3 = table2.lookup(table1, ("B", "==", "Fizz"), ("B", "==", "Buzz"), all=False)
+    assert list(table3.rows) == [('0', None, None, None),
+                                 ('1', 1, '1', '1'),
+                                 ('2', 2, '2', '2'),
+                                 ('3', 3, 'fizz', '3'),
+                                 ('4', 4, '4', '4'),
+                                 ('5', 5, '5', 'buzz'),
+                                 ('6', 6, 'fizz', '6'),
+                                 ('7', 7, '7', '7'),
+                                 ('8', 8, '8', '8'),
+                                 ('9', 9, 'fizz', '9'),
+                                 ('10', 10, '10', 'buzz'),
+                                 ('11', 11, '11', '11'),
+                                 ('12', 12, 'fizz', '12'),
+                                 ('13', 13, '13', '13'),
+                                 ('14', 14, '14', '14'),
+                                 ('15', None, None, None),
+                                 ('16', 16, '16', '16'),
+                                 ('17', 17, '17', '17'),
+                                 ('18', 18, 'fizz', '18'),
+                                 ('19', 19, '19', '19')]
+
+
+def test_sortation():  # Sortation
+    table7 = Table()
+    table7.add_column('A', int, data=[1, None, 8, 3, 4, 6, 5, 7, 9], allow_empty=True)
+    table7.add_column('B', int, data=[10, 100, 1, 1, 1, 1, 10, 10, 10])
+    table7.add_column('C', int, data=[0, 1, 0, 1, 0, 1, 0, 1, 0])
+
+    assert not table7.is_sorted()
+
+    sort_order = {'B': False, 'C': False, 'A': False}
+
+    table7.sort(**sort_order)
+
+    assert list(table7.rows) == [
+        (4, 1, 0),
+        (8, 1, 0),
+        (3, 1, 1),
+        (6, 1, 1),
+        (1, 10, 0),
+        (5, 10, 0),
+        (9, 10, 0),
+        (7, 10, 1),
+        (None, 100, 1)
+    ]
+
+    assert list(table7.filter('A', 'B', slice(4, 8))) == [(1, 10), (5, 10), (9, 10), (7, 10)]
+
+    assert table7.is_sorted(**sort_order)
+
+
+
+def test_lookup():
+    friends = Table()
+    friends.add_column("name", str, data=['Alice', 'Betty', 'Charlie', 'Dorethy', 'Edward', 'Fred'])
+    friends.add_column("stop", str, data=['Downtown-1', 'Downtown-2', 'Hillside View', 'Hillside Crescent', 'Downtown-2', 'Chicago'])
+    friends.show()
+
+    random.seed(11)
+    table_size = 40
+
+    times = [DataTypes.time(random.randint(21, 23), random.randint(0, 59)) for i in range(table_size)]
+    stops = ['Stadium', 'Hillside', 'Hillside View', 'Hillside Crescent', 'Downtown-1', 'Downtown-2',
+             'Central station'] * 2 + [f'Random Road-{i}' for i in range(table_size)]
+    route = [random.choice([1, 2, 3]) for i in stops]
+
+    bustable = Table()
+    bustable.add_column("time", DataTypes.time, data=times)
+    bustable.add_column("stop", str, data=stops[:table_size])
+    bustable.add_column("route", int, data=route[:table_size])
+
+    bustable.sort(**{'time': False})
+
+    print("Departures from Concert Hall towards ...")
+    bustable[:10].show()
+
+    lookup_1 = friends.lookup(bustable, (DataTypes.time(21, 10), "<=", 'time'), ('stop', "==", 'stop'))
+    lookup_1.sort(**{'time': False})
+    lookup_1.show()
+
+    expected = [
+        ('Fred', 'Chicago', None, None, None),
+        ('Dorethy', 'Hillside Crescent', time(23, 54), 'Hillside Crescent', 1),
+        ('Betty', 'Downtown-2', time(21, 51), 'Downtown-2', 1),
+        ('Edward', 'Downtown-2', time(21, 51), 'Downtown-2', 1),
+        ('Charlie', 'Hillside View', time(22, 19), 'Hillside View', 2),
+        ('Alice', 'Downtown-1', time(23, 12), 'Downtown-1', 3),
+    ]
+
+    for row in lookup_1.rows:
+        expected.remove(row)
+    assert expected == []
+
 
 def test_recreate_readme_comparison():  # TODO: Use cputils for getting the memory footprint.
     try:
