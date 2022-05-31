c00e3497846c0c21a07ef498a3e8c6f1aef9f5a2
root-11 <bjorn.h.madsen@gmail.com>
2022-05-31 12:31:11 +0100

    Added tests for Column.statistics

--- tablite/utils.py
+++ tablite/utils.py
@@ -1,4 +1,5 @@
 import math
+from datetime import datetime,date,time,timedelta
 
 
 def intercept(A,B):
@@ -48,3 +49,100 @@ def intercept(A,B):
     
     return range(start, end, step)
 
+
+def summary_statistics(values,counts):
+    """
+    values: any type
+    counts: integer
+
+    returns dict with:
+    - min (int/float, length of str, date)
+    - max (int/float, length of str, date)
+    - mean (int/float, length of str, date)
+    - median (int/float, length of str, date)
+    - stdev (int/float, length of str, date)
+    - mode (int/float, length of str, date)
+    - distinct (number of distinct values)
+    - iqr (int/float, length of str, date)
+    - sum (int/float, length of str, date)
+    - histogram
+    """
+    VC = [[v,c] for v,c in zip(values, counts)]
+    VC.sort()
+    vmin,vmax = VC[0][0], VC[-1][0]
+    
+    total_val, mode, median, total_cnt =  0, None, None, sum(counts)
+    
+    max_cnt, cnt_n =-1, 0
+    mn,c = 0, 0.0
+    iqr25 = total_cnt * 1/4
+    iqr50 = total_cnt * 1/2
+    iqr75 = total_cnt * 3/4
+    iqr_low, iqr_high = 0, 0
+    vx_0 = None
+    for vx, cx in VC:
+        if vx is None:
+            continue
+        elif isinstance(vx, (date, datetime, time, timedelta)):
+            continue
+        elif isinstance(vx, str):
+            vx = len(str)
+        elif isinstance(vx,(float,int)):
+            pass
+        else:
+            raise TypeError(vx)
+
+        cnt_0 = cnt_n
+        cnt_n += cx
+
+        if cnt_0 < iqr25 < cnt_n:
+            iqr_low = vx
+        elif cnt_0 == iqr25:
+            _,delta = divmod(1*(total_cnt-1), 4)
+            iqr_low = (vx_0 * (4-delta) + vx * delta) / 4
+
+        # median calculations
+        if cnt_n-cx < iqr50 < cnt_n:
+            median = vx
+        elif cnt_0 == iqr50:
+            _,delta = divmod(2*(total_cnt-1), 4)
+            median = (vx_0 * (4-delta) + vx * delta) / 4
+
+        if cnt_0 < iqr75 < cnt_n:
+            iqr_high = vx
+        elif cnt_0 == iqr75:
+            _,delta = divmod(3*(total_cnt-1), 4)
+            iqr_high = (vx_0 * (4-delta) + vx * delta) / 4
+        
+        # stdev calulations
+        # cnt = cnt_n  # self.count += 1
+        dt = cx * (vx-mn) # dt = value - self.mean
+        mn += dt / cnt_n  # self.mean += dt / self.count
+        c += dt * (vx-mn)  #self.c += dt * (value - self.mean)
+
+        # mode calculations
+        if cx > max_cnt:
+            mode,max_cnt = vx,cx
+
+        total_val += vx*cx
+        vx_0 = vx
+    
+    if cnt_n - 1 > 1:
+        var = c / (cnt_n-1)
+        stdev = var**(1/2)
+    
+    d = {
+        'min': vmin,
+        'max': vmax,
+        'mean': total_val / total_cnt,
+        'median': median,
+        'stdev': stdev,
+        'mode': mode,
+        'distinct': len(values),
+        'iqr_low': iqr_low,
+        'iqr_high': iqr_high,
+        'iqr': iqr_high-iqr_low,
+        'sum': total_val,
+        'histogram': [values,counts]
+    }
+    return d
