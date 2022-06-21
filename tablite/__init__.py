f1cb4751c7563ac36eef34436a77d707b6946ea6
root-11 <bjorn.h.madsen@gmail.com>
2022-06-21 12:57:31 +0100

    added groupby to init

--- tablite/__init__.py
+++ tablite/__init__.py
@@ -1,5 +1,6 @@
 from tablite.core import Table, Column #, GroupBy
 from tablite.datatypes import DataTypes
+from tablite.groupby_utils import GroupBy
 # __all__ = [i.__name__ for i in [Table, GroupBy]]
 
 
