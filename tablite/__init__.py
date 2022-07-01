99442c63fae10bc6760036e58875eb60c273ce2e
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-07-01 14:05:12 +0100

    removed __all__

--- tablite/__init__.py
+++ tablite/__init__.py
@@ -1,7 +1,6 @@
 from tablite.core import Table, Column #, GroupBy
 from tablite.datatypes import DataTypes
 from tablite.groupby_utils import GroupBy
-# __all__ = [i.__name__ for i in [Table, GroupBy]]
-
+from tablite.file_reader_utils import get_headers
 
 
