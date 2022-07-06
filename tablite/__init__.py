701865fff239626095c26636f0ac80d2d46c00c5
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-07-06 20:57:07 +0100

    added version to __init__

--- tablite/__init__.py
+++ tablite/__init__.py
@@ -1,6 +1,9 @@
-from tablite.core import Table, Column #, GroupBy
+from tablite.core import Table, Column 
 from tablite.datatypes import DataTypes
 from tablite.groupby_utils import GroupBy
 from tablite.file_reader_utils import get_headers
+
+__version__ = "2022.7.dev0"
+__author__ = "https://github.com/root-11"
 
 
