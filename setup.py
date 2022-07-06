0a7570da5568239579216878e96e342d0cafeddb
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-07-06 19:51:57 +0100

    feature15 amendments.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 """
 tablite
 """
-build_tag = "1fc7327e8d5f70396040669acfca710e1caf35e9fcfefe77f1506242da8b7856"
 from setuptools import setup
 from pathlib import Path
 
@@ -28,15 +27,14 @@ keywords.sort(key=lambda x: x.lower())
 with open('requirements.txt', 'r') as fi:
     requirements = [v.rstrip('\n') for v in fi.readlines()]
 
-
 setup(
     name="tablite",
-    version="2022.2.5.67057",
+    version="2022.07dev0",  # <----------- double check!
     url="https://github.com/root-11/tablite",
     license="MIT",
     author="Bjorn Madsen",
     author_email="bjorn.madsen@operationsresearchgroup.com",
-    description="A table crunching library",
+    description="A library for cleaning tabular data.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     keywords=keywords,
@@ -52,6 +50,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
 
