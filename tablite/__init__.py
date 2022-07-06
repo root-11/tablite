4db16693a93760530f14fa87d89f06073b2bbcf5
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-07-06 21:36:29 +0100

    update to version

--- tablite/__init__.py
+++ tablite/__init__.py
@@ -1,9 +1,9 @@
-from tablite.core import Table, Column 
-from tablite.datatypes import DataTypes
-from tablite.groupby_utils import GroupBy
-from tablite.file_reader_utils import get_headers
+from .core import Table, Column 
+from .datatypes import DataTypes
+from .groupby_utils import GroupBy
+from .file_reader_utils import get_headers
+from .version import __version__
 
-__version__ = "2022.7.dev0"
-__author__ = "https://github.com/root-11"
+
 
 
