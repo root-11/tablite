b92280f41f2d9743f90919b9a95159e793370593
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-10-19 12:02:13 +0100

    Removed `import_as` from all tests.

--- tests/test_to_json.py
+++ tests/test_to_json.py
@@ -3,8 +3,14 @@ from pathlib import Path
 import pytest
 
 
+@pytest.fixture(autouse=True) # this resets the HDF5 file for every test.
+def refresh():
+    Table.reset_storage()
+    yield
+
+
 def test_defaults():
-    tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv", import_as='csv')
+    tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
     d = tbl.to_dict()  # defaults.
     assert d['columns']
     for name in tbl.columns:
@@ -15,8 +21,9 @@ def test_defaults():
     assert "row id" in d['columns']
     assert "row id" not in tbl.columns
 
+
 def test_no_row_count():
-    tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv", import_as='csv')
+    tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
     d = tbl.to_dict(row_count=None)  
 
     assert len(d['columns']) == len(tbl.columns)
@@ -24,9 +31,10 @@ def test_no_row_count():
         assert name in d['columns']
         assert d['columns'][name] == tbl[name]
     assert d['total_rows'] == len(tbl)
-    
+
+
 def test_limited_columns_and_slice():
-    tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv", import_as='csv')
+    tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
     columns=["Id","Client","Product"]
     slcs = slice(3,100,9)
     d = tbl.to_dict(columns=columns, slice_=slcs, row_count='rid')
@@ -41,7 +49,7 @@ def test_limited_columns_and_slice():
 
 
 def test_defaults_to_json_and_back():
-    tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv", import_as='csv')
+    tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
     s = tbl.to_json()  # just use defaults.
     tbl2 = Table.from_json(s)
     assert all(c in tbl2.columns for c in tbl.columns)
