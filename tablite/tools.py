e25411b9199f6b04774f72a9ee36ce80a6044102
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-10-22 15:50:46 +0100

    updated imports for tools.

--- tablite/tools.py
+++ tablite/tools.py
@@ -1,15 +1,26 @@
 # tools.py is a collection of helpers inspired by Python collections.
 
-from datatypes import DataTypes
-from utils import date_range  # helper for date_ranges
-from utils import xround
+from tablite.datatypes import DataTypes
+from tablite.utils import date_range  # helper for date_ranges
+from tablite.utils import xround
 
 assert callable(date_range)
 assert callable(xround)
 
 
 def guess(values):
-    """ guesses datatypes"""
+    """ 
+    guesses datatypes 
+
+    example:
+    >>> guess(['1','2','3'])
+    [1,2,3]
+    >>> tools.guess(['1.0','2.0','3.0'])
+    [1.0, 2.0, 3.0]
+    >>> guess(['true', 'false'])
+    [True,False]
+    
+    """
     return DataTypes.guess(values)
 
 
