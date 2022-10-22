413a9e209abac266a03801bc5623487b78047386
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-10-22 13:10:05 +0100

    Updated table.to_dict() to behave pythonically. Updated table.to_json_serializable maintain its current api promise.

--- tests/test_to_json.py
+++ tests/test_to_json.py
@@ -12,41 +12,36 @@ def refresh():
 def test_defaults():
     tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
     d = tbl.to_dict()  # defaults.
-    assert d['columns']
     for name in tbl.columns:
-        assert name in d['columns']
-        assert d['columns'][name] == tbl[name]
-    assert d['total_rows'] == len(tbl)
-
-    assert "row id" in d['columns']
-    assert "row id" not in tbl.columns
-
+        assert name in d
+        assert d[name] == tbl[name]
 
 def test_no_row_count():
     tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
-    d = tbl.to_dict(row_count=None)  
+    d = tbl.to_dict()  
 
-    assert len(d['columns']) == len(tbl.columns)
     for name in tbl.columns:
-        assert name in d['columns']
-        assert d['columns'][name] == tbl[name]
-    assert d['total_rows'] == len(tbl)
+        assert name in d
+        assert d[name] == tbl[name]
+
+    jsn_d = tbl.as_json_serializable()
+    assert isinstance(jsn_d, dict)
+    for k,v in d.items():
+        len(jsn_d['columns'][k]) == len(v)
+    
+    assert jsn_d["total_rows"] == len(tbl)
 
 
 def test_limited_columns_and_slice():
     tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
     columns=["Id","Client","Product"]
     slcs = slice(3,100,9)
-    d = tbl.to_dict(columns=columns, slice_=slcs, row_count='rid')
-    
-    assert len(d['columns']) == len(columns) + 1  # 1 for row id.
-    L = list(range(*slcs.indices(len(tbl))))
-    for _, data in d['columns'].items():
-         assert len(data) == len(L)
-    assert d['columns']['rid'] == [1 + i for i in L]  # start_on = 1
-    
-    assert d['total_rows'] == len(tbl)
+    d = tbl.to_dict(columns=columns, slice_=slcs)
 
+    L = list(range(*slcs.indices(len(tbl))))
+    for _, data in d.items():
+         assert len(data) == len(L)
+    
 
 def test_defaults_to_json_and_back():
     tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
@@ -54,4 +49,5 @@ def test_defaults_to_json_and_back():
     tbl2 = Table.from_json(s)
     assert all(c in tbl2.columns for c in tbl.columns)
     assert len(tbl2.columns) == len(tbl.columns) + 1
+    
     
