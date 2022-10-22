1a64e7de6ba7578b5636843460bcb644c06e07f8
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-10-22 16:09:15 +0100

    completed tool collection

--- tablite/tools.py
+++ tablite/tools.py
@@ -3,24 +3,25 @@
 from tablite.datatypes import DataTypes
 from tablite.utils import date_range  # helper for date_ranges
 from tablite.utils import xround
+from tablite.utils import unique_name 
+from tablite.utils import intercept as range_intercept
+from tablite.sortation import text_sort, unix_sort, excel_sort
+from tablite.file_reader_utils import detect_seperator, get_encoding, get_headers, get_delimiter
+
 
 assert callable(date_range)
 assert callable(xround)
+assert callable(unique_name)
+assert callable(range_intercept)
+assert callable(text_sort)
+assert callable(unix_sort)
+assert callable(excel_sort)
 
+assert callable(detect_seperator)
+assert callable(get_encoding)
+assert callable(get_headers)
+assert callable(get_delimiter)
 
-def guess(values):
-    """ 
-    guesses datatypes 
-
-    example:
-    >>> guess(['1','2','3'])
-    [1,2,3]
-    >>> tools.guess(['1.0','2.0','3.0'])
-    [1.0, 2.0, 3.0]
-    >>> guess(['true', 'false'])
-    [True,False]
-    
-    """
-    return DataTypes.guess(values)
-
+guess = DataTypes.guess
+xround = DataTypes.xround
 
