52cf271d415ae44a9d2379a5bdf68e47301a2bab
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-11-22 18:59:20 +0000

    black'd & flake8'd

--- tests/test_imputation.py
+++ tests/test_imputation.py
@@ -2,87 +2,67 @@ from tablite import Table
 import pytest
 
 
-@pytest.fixture(autouse=True) # this resets the HDF5 file for every test.
+@pytest.fixture(autouse=True)  # this resets the HDF5 file for every test.
 def refresh():
     Table.reset_storage()
     yield
 
 
 def test_replace_missing_values_00():
-    sample = [
-        [1, 2, 3],
-        [1, 2, None],
-        [5, 5, 5],
-        [6, 6, 6]
-        ]
+    sample = [[1, 2, 3], [1, 2, None], [5, 5, 5], [6, 6, 6]]
 
     t = Table()
-    t.add_columns(*list('abc'))
+    t.add_columns(*list("abc"))
     for row in sample:
         t.add_rows(row)
-    
+
     expected = [r[:] for r in sample]
     expected[1][-1] = 3
-    result = t.imputation(sources=['a','b'], targets=['c'], method='nearest neighbour', missing=None)
+    result = t.imputation(sources=["a", "b"], targets=["c"], method="nearest neighbour", missing=None)
     assert [r for r in result.rows] == expected
 
 
 def test_replace_missing_values_01():
-    sample = [
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, None, None],
-        [5, 5, 5],
-        [6, 6, 6]
-    ]
-    
+    sample = [[1, 2, 3], [1, 2, 3], [1, None, None], [5, 5, 5], [6, 6, 6]]
+
     t = Table()
-    t.add_columns(*list('abc'))
+    t.add_columns(*list("abc"))
     for row in sample:
         t.add_rows(row)
-    
+
     expected = [r[:] for r in sample]
     expected[2][1] = 2
     expected[2][2] = 3
-    # result = substitute_missing_values(sample)
-    result = t.imputation(sources=['a','b'], targets=['b','c'], method='nearest neighbour', missing=None)
+    
+    result = t.imputation(sources=["a", "b"], targets=["b", "c"], method="nearest neighbour", missing=None)
     assert [r for r in result.rows] == expected
 
-    result = t.imputation(sources=['a','b','c'], targets=['a','b','c'], method='nearest neighbour', missing=None)
+    result = t.imputation(sources=["a", "b", "c"], targets=["a", "b", "c"], method="nearest neighbour", missing=None)
     assert [r for r in result.rows] == expected
 
 
 def test_replace_missing_values_02():
-    sample = [
-        [1, 2, None],
-        [5, 5, 5],
-        [6, 6, 6]
-    ]
+    sample = [[1, 2, None], [5, 5, 5], [6, 6, 6]]
 
     t = Table()
-    t.add_columns(*list('abc'))
+    t.add_columns(*list("abc"))
     for row in sample:
         t.add_rows(row)
 
     expected = [r[:] for r in sample]
     expected[0][-1] = 5
-    result = t.imputation(targets=['c'], sources=['a','b'], method='nearest neighbour')
+    result = t.imputation(targets=["c"], sources=["a", "b"], method="nearest neighbour")
     assert [r for r in result.rows] == expected
 
-    result = t.imputation(targets=['c'], method='nearest neighbour')
+    result = t.imputation(targets=["c"], method="nearest neighbour")
     assert [r for r in result.rows] == expected
 
 
 def test_replace_missing_values_02b():
-    sample = [
-        [1, 2, None],
-        [5, 5, None],
-        [5, 5, 5],
-        [6, 6, 6]
-    ]
+    sample = [[1, 2, None], [5, 5, None], [5, 5, 5], [6, 6, 6]]
 
     t = Table()
-    t.add_columns(*list('abc'))
+    t.add_columns(*list("abc"))
     for row in sample:
         t.add_rows(row)
 
@@ -90,21 +70,15 @@ def test_replace_missing_values_02b():
     expected[0][-1] = 5
     expected[1][-1] = 5
 
-    result = t.imputation(targets=['c'], method='nearest neighbour')
+    result = t.imputation(targets=["c"], method="nearest neighbour")
     assert [r for r in result.rows] == expected
 
 
 def test_replace_missing_values_02_same_row_twice():
-    sample = [
-        [1, 2, None],
-        [1, 2, None],
-        [5, 5, None],
-        [5, 5, 5],
-        [6, 6, 6]
-    ]
+    sample = [[1, 2, None], [1, 2, None], [5, 5, None], [5, 5, 5], [6, 6, 6]]
 
     t = Table()
-    t.add_columns(*list('abc'))
+    t.add_columns(*list("abc"))
     for row in sample:
         t.add_rows(row)
 
@@ -113,26 +87,21 @@ def test_replace_missing_values_02_same_row_twice():
     expected[1][-1] = 5
     expected[2][-1] = 5
 
-    result = t.imputation(targets=['c'], method='nearest neighbour')
+    result = t.imputation(targets=["c"], method="nearest neighbour")
     assert [r for r in result.rows] == expected
 
 
 def test_replace_missing_values_05():
-    sample = [
-        [None, 1, 2, 3],
-        [0, None, 2, 3],
-        [0, 1, None, 3],
-        [0, 1, 2, None]
-    ]
+    sample = [[None, 1, 2, 3], [0, None, 2, 3], [0, 1, None, 3], [0, 1, 2, None]]
 
-    cols = [str(i) for i,_ in enumerate(sample[0])]
+    cols = [str(i) for i, _ in enumerate(sample[0])]
     t = Table()
     t.add_columns(*cols)
     for row in sample:
         t.add_rows(row)
 
-    result = t.imputation(targets=cols, method='nearest neighbour', sources=cols)
-    result = result.imputation(targets=cols, method='nearest neighbour', sources=cols)
+    result = t.imputation(targets=cols, method="nearest neighbour", sources=cols)
+    result = result.imputation(targets=cols, method="nearest neighbour", sources=cols)
 
     expected = [
         [0, 1, 2, 3],
@@ -145,21 +114,16 @@ def test_replace_missing_values_05():
 
 
 def test_replace_missing_values_06():
-    sample = [
-        [None, None, 2, 3],
-        [0, None, None, 3],
-        [0, 1, None, None],
-        [0, 1, 2, None]
-    ]
+    sample = [[None, None, 2, 3], [0, None, None, 3], [0, 1, None, None], [0, 1, 2, None]]
 
-    cols = [str(i) for i,_ in enumerate(sample[0])]
+    cols = [str(i) for i, _ in enumerate(sample[0])]
     t = Table()
     t.add_columns(*cols)
     for row in sample:
         t.add_rows(row)
 
-    result = t.imputation(targets=cols, method='nearest neighbour', sources=cols)
-    result = result.imputation(targets=cols, method='nearest neighbour', sources=cols)
+    result = t.imputation(targets=cols, method="nearest neighbour", sources=cols)
+    result = result.imputation(targets=cols, method="nearest neighbour", sources=cols)
 
     expected = [
         [0, 1, 2, 3],
@@ -172,72 +136,51 @@ def test_replace_missing_values_06():
 
 
 def test_replace_missing_values_07():
-    sample = [
-        [1, 1, 1, 1, 1, 1],
-        [2, 2, 2, 2, 2, 2],
-        [3, 3, 3, 3, 3, None]
-    ]
+    sample = [[1, 1, 1, 1, 1, 1], [2, 2, 2, 2, 2, 2], [3, 3, 3, 3, 3, None]]
 
-    cols = [str(i) for i,_ in enumerate(sample[0])]
+    cols = [str(i) for i, _ in enumerate(sample[0])]
     t = Table()
     t.add_columns(*cols)
     for row in sample:
         t.add_rows(row)
 
-    result = t.imputation(targets=cols, method='nearest neighbour', sources=cols)
+    result = t.imputation(targets=cols, method="nearest neighbour", sources=cols)
 
-    expected = [
-        [1, 1, 1, 1, 1, 1],
-        [2, 2, 2, 2, 2, 2],
-        [3, 3, 3, 3, 3, 2]
-    ]
+    expected = [[1, 1, 1, 1, 1, 1], [2, 2, 2, 2, 2, 2], [3, 3, 3, 3, 3, 2]]
     # special case: columns 1 to 5 are unique, so they become indices.
     # the missing value is then subtituted with first match.
     assert [r for r in result.rows] == expected
 
 
 def test_replace_missing_values_09():
-    sample = [
-        ['1', 2, True, 'this'],
-        ['2', 2, False, 'that'],
-        ['3', 1, True, 'that']
-    ]
+    sample = [["1", 2, True, "this"], ["2", 2, False, "that"], ["3", 1, True, "that"]]
 
-    cols = [str(i) for i,_ in enumerate(sample[0])]
+    cols = [str(i) for i, _ in enumerate(sample[0])]
     t = Table()
     t.add_columns(*cols)
     for row in sample:
         t.add_rows(row)
 
-    result = t.imputation(targets=cols, method='nearest neighbour', sources=cols)
+    result = t.imputation(targets=cols, method="nearest neighbour", sources=cols)
 
     assert [r for r in result.rows] == sample
 
 
 def test_replace_missing_values_10():
-    sample = [
-        ['1', 2, False, 'this'],
-        ['2', 2, False, 'that'],
-        ['3', 1, True, 'that'],
-        ['4', 1, True, None]
-    ]   
+    sample = [["1", 2, False, "this"], ["2", 2, False, "that"], ["3", 1, True, "that"], ["4", 1, True, None]]
 
-    cols = [str(i) for i,_ in enumerate(sample[0])]
+    cols = [str(i) for i, _ in enumerate(sample[0])]
     t = Table()
     t.add_columns(*cols)
     for row in sample:
         t.add_rows(row)
 
-    result = t.imputation(targets=cols, method='nearest neighbour', sources=cols)
+    result = t.imputation(targets=cols, method="nearest neighbour", sources=cols)
 
-    expected = [
-        ['1', 2, False, 'this'],
-        ['2', 2, False, 'that'],
-        ['3', 1, True, 'that'],
-        ['4', 1, True, 'that']
-    ]
+    expected = [["1", 2, False, "this"], ["2", 2, False, "that"], ["3", 1, True, "that"], ["4", 1, True, "that"]]
 
     assert [r for r in result.rows] == expected
+
 
 def test_replace_missing_values_03():
     sample = [
@@ -252,15 +195,15 @@ def test_replace_missing_values_03():
         [4, 4, 2, 3, 4],
         [4, 2, 1, 1, 2],
         [4, 4, 4, 1, 1],
-        [3, 3, 4, 1, None]
+        [3, 3, 4, 1, None],
     ]
 
     t = Table()
-    t.add_columns(*list('abcde'))
+    t.add_columns(*list("abcde"))
     for row in sample:
         t.add_rows(row)
 
-    result = t.imputation(targets=['d','e'], method='nearest neighbour', sources=list('abc'))
+    result = t.imputation(targets=["d", "e"], method="nearest neighbour", sources=list("abc"))
 
     expected = [
         [1, 2, 1, 1, 1],
@@ -274,45 +217,49 @@ def test_replace_missing_values_03():
         [4, 4, 2, 3, 4],
         [4, 2, 1, 1, 2],
         [4, 4, 4, 1, 1],
-        [3, 3, 4, 1, 3]
+        [3, 3, 4, 1, 3],
     ]
 
     assert [r for r in result.rows] == expected
 
 
 def test_replace_missing_values_04():
-    sample = [[5, None, 9, None, 5, 9, 3, 3, 9, None, 8, 6, 5, 4, 2, 8, 9, 3, 9, 9],
-              [7, None, None, 4, 7, None, 8, 8, 4, 1, 4, 1, 1, 1, 2, 5, 4, 10, 4, 7],
-              [2, 6, 8, 1, 2, 6, 2, 4, 3, 5, 2, 9, 9, 2, None, 3, 8, 7, None, 1],
-              [None, 8, 9, 8, 5, 1, 9, 6, 10, 9, 9, 3, 2, 4, 10, 10, 2, 10, 6, None],
-              [6, 7, 5, 2, 10, None, None, 4, 3, None, 4, 3, 9, 10, None, 9, 9, 2, None, 2],
-              [9, 5, 6, 5, 5, None, 2, 5, 6, 5, 8, 7, 2, 3, 6, 3, 1, 1, 4, 2],
-              [9, 7, None, 4, 3, 1, 6, 7, 4, 2, 1, 4, 8, 4, 4, 5, 5, 9, 9, 1],
-              [5, 5, 10, 6, 9, 10, 1, 1, 9, 4, 2, None, None, 2, 5, 7, 5, 4, 7, 3],
-              [5, 1, 4, 10, 6, 8, 9, 3, 5, 10, 4, 4, 3, 4, None, 2, 10, 7, 4, 1],
-              [2, 4, 9, 9, 2, 8, 9, 8, 9, 9, 3, 8, 4, 5, 9, 4, 2, 4, 6, 2],
-              [4, 3, 2, 6, 7, 7, 10, None, 10, 1, None, 7, 7, 9, 4, 5, 1, 7, 8, None],
-              [None, 7, 1, 7, 8, 5, 4, 6, 7, 7, 2, 9, 6, 2, 1, 4, 1, 4, 2, 4]]
+    sample = [
+        [5, None, 9, None, 5, 9, 3, 3, 9, None, 8, 6, 5, 4, 2, 8, 9, 3, 9, 9],
+        [7, None, None, 4, 7, None, 8, 8, 4, 1, 4, 1, 1, 1, 2, 5, 4, 10, 4, 7],
+        [2, 6, 8, 1, 2, 6, 2, 4, 3, 5, 2, 9, 9, 2, None, 3, 8, 7, None, 1],
+        [None, 8, 9, 8, 5, 1, 9, 6, 10, 9, 9, 3, 2, 4, 10, 10, 2, 10, 6, None],
+        [6, 7, 5, 2, 10, None, None, 4, 3, None, 4, 3, 9, 10, None, 9, 9, 2, None, 2],
+        [9, 5, 6, 5, 5, None, 2, 5, 6, 5, 8, 7, 2, 3, 6, 3, 1, 1, 4, 2],
+        [9, 7, None, 4, 3, 1, 6, 7, 4, 2, 1, 4, 8, 4, 4, 5, 5, 9, 9, 1],
+        [5, 5, 10, 6, 9, 10, 1, 1, 9, 4, 2, None, None, 2, 5, 7, 5, 4, 7, 3],
+        [5, 1, 4, 10, 6, 8, 9, 3, 5, 10, 4, 4, 3, 4, None, 2, 10, 7, 4, 1],
+        [2, 4, 9, 9, 2, 8, 9, 8, 9, 9, 3, 8, 4, 5, 9, 4, 2, 4, 6, 2],
+        [4, 3, 2, 6, 7, 7, 10, None, 10, 1, None, 7, 7, 9, 4, 5, 1, 7, 8, None],
+        [None, 7, 1, 7, 8, 5, 4, 6, 7, 7, 2, 9, 6, 2, 1, 4, 1, 4, 2, 4],
+    ]
 
-    cols = [str(i) for i,_ in enumerate(sample[0])]
+    cols = [str(i) for i, _ in enumerate(sample[0])]
     t = Table()
     t.add_columns(*cols)
     for row in sample:
         t.add_rows(row)
 
-    result = t.imputation(targets=cols, method='nearest neighbour', sources=cols)
+    result = t.imputation(targets=cols, method="nearest neighbour", sources=cols)
 
-    expected = [[5, 4, 9, 9, 5, 9, 3, 3, 9, 9, 8, 6, 5, 4, 2, 8, 9, 3, 9, 9],
-                [7, 7, 9, 4, 7, 1, 8, 8, 4, 1, 4, 1, 1, 1, 2, 5, 4, 10, 4, 7],
-                [2, 6, 8, 1, 2, 6, 2, 4, 3, 5, 2, 9, 9, 2, 9, 3, 8, 7, 6, 1],
-                [2, 8, 9, 8, 5, 1, 9, 6, 10, 9, 9, 3, 2, 4, 10, 10, 2, 10, 6, 2],
-                [6, 7, 5, 2, 10, 6, 2, 4, 3, 5, 4, 3, 9, 10, 6, 9, 9, 2, 4, 2],
-                [9, 5, 6, 5, 5, 8, 2, 5, 6, 5, 8, 7, 2, 3, 6, 3, 1, 1, 4, 2],
-                [9, 7, 9, 4, 3, 1, 6, 7, 4, 2, 1, 4, 8, 4, 4, 5, 5, 9, 9, 1],
-                [5, 5, 10, 6, 9, 10, 1, 1, 9, 4, 2, 8, 4, 2, 5, 7, 5, 4, 7, 3],
-                [5, 1, 4, 10, 6, 8, 9, 3, 5, 10, 4, 4, 3, 4, 9, 2, 10, 7, 4, 1],
-                [2, 4, 9, 9, 2, 8, 9, 8, 9, 9, 3, 8, 4, 5, 9, 4, 2, 4, 6, 2],
-                [4, 3, 2, 6, 7, 7, 10, 8, 10, 1, 3, 7, 7, 9, 4, 5, 1, 7, 8, 2],
-                [2, 7, 1, 7, 8, 5, 4, 6, 7, 7, 2, 9, 6, 2, 1, 4, 1, 4, 2, 4]]
+    expected = [
+        [5, 4, 9, 9, 5, 9, 3, 3, 9, 9, 8, 6, 5, 4, 2, 8, 9, 3, 9, 9],
+        [7, 7, 9, 4, 7, 1, 8, 8, 4, 1, 4, 1, 1, 1, 2, 5, 4, 10, 4, 7],
+        [2, 6, 8, 1, 2, 6, 2, 4, 3, 5, 2, 9, 9, 2, 9, 3, 8, 7, 6, 1],
+        [2, 8, 9, 8, 5, 1, 9, 6, 10, 9, 9, 3, 2, 4, 10, 10, 2, 10, 6, 2],
+        [6, 7, 5, 2, 10, 6, 2, 4, 3, 5, 4, 3, 9, 10, 6, 9, 9, 2, 4, 2],
+        [9, 5, 6, 5, 5, 8, 2, 5, 6, 5, 8, 7, 2, 3, 6, 3, 1, 1, 4, 2],
+        [9, 7, 9, 4, 3, 1, 6, 7, 4, 2, 1, 4, 8, 4, 4, 5, 5, 9, 9, 1],
+        [5, 5, 10, 6, 9, 10, 1, 1, 9, 4, 2, 8, 4, 2, 5, 7, 5, 4, 7, 3],
+        [5, 1, 4, 10, 6, 8, 9, 3, 5, 10, 4, 4, 3, 4, 9, 2, 10, 7, 4, 1],
+        [2, 4, 9, 9, 2, 8, 9, 8, 9, 9, 3, 8, 4, 5, 9, 4, 2, 4, 6, 2],
+        [4, 3, 2, 6, 7, 7, 10, 8, 10, 1, 3, 7, 7, 9, 4, 5, 1, 7, 8, 2],
+        [2, 7, 1, 7, 8, 5, 4, 6, 7, 7, 2, 9, 6, 2, 1, 4, 1, 4, 2, 4],
+    ]
 
     assert [r for r in result.rows] == expected
