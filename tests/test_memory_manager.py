83cfc0a47e62db97a92396957450b77ba1ab4638
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-11-22 18:54:00 +0000

    more black.

--- tests/test_memory_manager.py
+++ tests/test_memory_manager.py
@@ -1,20 +1,19 @@
-
 from tablite.memory_manager import timeout
 import time
 
 
 @timeout
 def sleepy(n):
-    time.sleep(n/10)
+    time.sleep(n / 10)
     return n
+
 
 @timeout
 def blowup():
     raise OSError()
 
 
-def test_timeout_result():  
-    for i in [1,3]:
+def test_timeout_result():
+    for i in [1, 3]:
         x = sleepy(i)  # tests that the timeout decorator works on sleepy
         assert x == i
-
