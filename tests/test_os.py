9d53036d1c090486aa4762b1325c5b8616dc3cfb
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-11-22 18:50:39 +0000

    black'd

--- tests/test_os.py
+++ tests/test_os.py
@@ -18,16 +18,16 @@ def test2():
         path.unlink()
 
     key = "/table/1"
-    with h5py.File(path, 'a') as h5:
-        dset = h5.create_dataset(name=key, dtype=h5py.Empty('f'))
-        dset.attrs['now'] = time.time()
-    
-    with h5py.File(path, 'w') as h5:
+    with h5py.File(path, "a") as h5:
+        dset = h5.create_dataset(name=key, dtype=h5py.Empty("f"))
+        dset.attrs["now"] = time.time()
+
+    with h5py.File(path, "w") as h5:
         assert list(h5.keys()) == []
 
-    with h5py.File(path, 'r+') as h5:
+    with h5py.File(path, "r+") as h5:
         assert key not in h5.keys()
-        dset = h5.create_dataset(name=key, dtype=h5py.Empty('f'))
-        dset.attrs['now'] = time.time()
-    
+        dset = h5.create_dataset(name=key, dtype=h5py.Empty("f"))
+        dset.attrs["now"] = time.time()
+
     path.unlink()
