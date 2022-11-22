fc363913bceebe6ae114a70d781ac3d2b55c7e31
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-11-22 18:35:21 +0000

    black'd

--- tests/test_transpose.py
+++ tests/test_transpose.py
@@ -1,38 +1,38 @@
 from tablite import Table
 
+
 def test01():
     t = Table()
-    t['a'] = [1]
-    t['b'] = [2]
-    t['c'] = [3]
-    t['d'] = [4]
-    t['e'] = [5]
+    t["a"] = [1]
+    t["b"] = [2]
+    t["c"] = [3]
+    t["d"] = [4]
+    t["e"] = [5]
 
-    new = t.transpose(columns=['c','d','e'], keep=['a','b'])
+    new = t.transpose(columns=["c", "d", "e"], keep=["a", "b"])
 
     assert [r for r in new.rows] == [
-        [1,2,'c',3],
-        [1,2,'d',4],
-        [1,2,'e',5],
+        [1, 2, "c", 3],
+        [1, 2, "d", 4],
+        [1, 2, "e", 5],
     ]
 
 
 def test02():
     t = Table()
-    t['a'] = [1,10]
-    t['b'] = [2,20]
-    t['c'] = [3,30]
-    t['d'] = [4,40]
-    t['e'] = [5,50]
+    t["a"] = [1, 10]
+    t["b"] = [2, 20]
+    t["c"] = [3, 30]
+    t["d"] = [4, 40]
+    t["e"] = [5, 50]
 
-    new = t.transpose(columns=['c','d','e'], keep=['a','b'])
+    new = t.transpose(columns=["c", "d", "e"], keep=["a", "b"])
 
     assert [r for r in new.rows] == [
-        [1,2,'c',3],
-        [1,2,'d',4],
-        [1,2,'e',5],
-        [10,20,'c',30],
-        [10,20,'d',40],
-        [10,20,'e',50],
-    ]    
-
+        [1, 2, "c", 3],
+        [1, 2, "d", 4],
+        [1, 2, "e", 5],
+        [10, 20, "c", 30],
+        [10, 20, "d", 40],
+        [10, 20, "e", 50],
+    ]
