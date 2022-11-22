da2832910191aad695d1fb507cd3b45fd2836295
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-11-22 18:35:54 +0000

    updated formatting with black

--- tests/test_to_json.py
+++ tests/test_to_json.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 
 
-@pytest.fixture(autouse=True) # this resets the HDF5 file for every test.
+@pytest.fixture(autouse=True)  # this resets the HDF5 file for every test.
 def refresh():
     Table.reset_storage()
     yield
@@ -16,9 +16,10 @@ def test_defaults():
         assert name in d
         assert d[name] == tbl[name]
 
+
 def test_no_row_count():
     tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
-    d = tbl.to_dict()  
+    d = tbl.to_dict()
 
     for name in tbl.columns:
         assert name in d
@@ -26,22 +27,22 @@ def test_no_row_count():
 
     jsn_d = tbl.as_json_serializable()
     assert isinstance(jsn_d, dict)
-    for k,v in d.items():
-        len(jsn_d['columns'][k]) == len(v)
-    
+    for k, v in d.items():
+        len(jsn_d["columns"][k]) == len(v)
+
     assert jsn_d["total_rows"] == len(tbl)
 
 
 def test_limited_columns_and_slice():
     tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
-    columns=["Id","Client","Product"]
-    slcs = slice(3,100,9)
+    columns = ["Id", "Client", "Product"]
+    slcs = slice(3, 100, 9)
     d = tbl.to_dict(columns=columns, slice_=slcs)
 
     L = list(range(*slcs.indices(len(tbl))))
     for _, data in d.items():
-         assert len(data) == len(L)
-    
+        assert len(data) == len(L)
+
 
 def test_defaults_to_json_and_back():
     tbl = Table.import_file(Path(__file__).parent / "data" / "ols.csv")
@@ -49,5 +50,3 @@ def test_defaults_to_json_and_back():
     tbl2 = Table.from_json(s)
     assert all(c in tbl2.columns for c in tbl.columns)
     assert len(tbl2.columns) == len(tbl.columns) + 1
-    
-    
