ccc326632324129b3976e94ec5c19ecb819fb002
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-11-22 18:36:13 +0000

    black'd update

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -1,4 +1,4 @@
-from mplite import TaskManager,Task
+from mplite import TaskManager, Task
 import time
 
 
@@ -6,10 +6,11 @@ def test_main():
     args = list(range(10)) * 5
     start = time.time()
     with TaskManager() as tm:
-        tasks = [Task(f, *(arg/10,), **{'hello': arg}) for arg in args]
+        tasks = [Task(f, *(arg / 10,), **{"hello": arg}) for arg in args]
         results = tm.execute(tasks)
     end = time.time()
     print(f"did nothing for {end-start} seconds, producing {results} as result")
+
 
 def f(*args, **kwargs):
     print(args, kwargs)
