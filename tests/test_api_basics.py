3b10118573aa396a14ca7fbb96f33766467c3e45
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-12-05 09:12:38 +0000

    Missing assert

--- tests/test_api_basics.py
+++ tests/test_api_basics.py
@@ -580,7 +580,7 @@ def test04_utf8_extension():
     for row in t.rows:
         t2.add_rows(row)
     t2.show()
-    t == t2
+    assert t == t2
 
 
 def test04_verify_multiprocessing_index_in_shared_memory():
