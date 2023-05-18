beb2e72ec08ed394c5d436eb2e07ff67dd5dda4f
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2023-05-18 18:50:40 +0100

    Refactored multiprocessing tasks into mp_utils.

--- tablite/core.py
+++ tablite/core.py
@@ -1,13 +1,6 @@
-import logging
-import numpy as np
-
-# import pathlib
-from pathlib import Path
 import sys
-import operator
-
-from string import digits
-from multiprocessing import shared_memory
+import logging
+from pathlib import Path
 
 from tqdm import tqdm as _tqdm
 
@@ -340,7 +333,7 @@ class Table(BaseTable):
         param: sort_mode: str: "alphanumeric", "unix", or, "excel" (default)
         param: **kwargs: sort criteria. See Table.sort()
         """
-        return reorder.sort_index(self, sort_mode="excel", tqdm=_tqdm, pbar=None, **kwargs)
+        return reorder.sort_index(self, sort_mode, tqdm=_tqdm, pbar=None, **kwargs)
 
     def reindex(self, index):
         """
@@ -590,7 +583,7 @@ class Table(BaseTable):
         # +===+===+========+=====+=====+=====+
 
         """
-        return pivots.pivot(self, rows, columns, functions, values_as_rows=True, tqdm=_tqdm, pbar=None)
+        return pivots.pivot(self, rows, columns, functions, values_as_rows, tqdm=_tqdm, pbar=None)
 
     def join(self, other, left_keys, right_keys, left_columns, right_columns, kind="inner", tqdm=_tqdm, pbar=None):
         """
@@ -623,7 +616,7 @@ class Table(BaseTable):
         )
         """
         return joins.left_join(
-            self, other, left_keys, right_keys, left_columns=None, right_columns=None, tqdm=_tqdm, pbar=None
+            self, other, left_keys, right_keys, left_columns, right_columns, tqdm=_tqdm, pbar=None
         )
 
     def inner_join(self, other, left_keys, right_keys, left_columns=None, right_columns=None, tqdm=_tqdm, pbar=None):
@@ -641,7 +634,7 @@ class Table(BaseTable):
             )
         """
         return joins.inner_join(
-            self, other, left_keys, right_keys, left_columns=None, right_columns=None, tqdm=_tqdm, pbar=None
+            self, other, left_keys, right_keys, left_columns, right_columns, tqdm=_tqdm, pbar=None
         )
 
     def outer_join(self, other, left_keys, right_keys, left_columns=None, right_columns=None, tqdm=_tqdm, pbar=None):
@@ -659,7 +652,7 @@ class Table(BaseTable):
             )
         """
         return joins.outer_join(
-            self, other, left_keys, right_keys, left_columns=None, right_columns=None, tqdm=_tqdm, pbar=None
+            self, other, left_keys, right_keys, left_columns, right_columns, tqdm=_tqdm, pbar=None
         )
 
     def cross_join(self, other, left_keys, right_keys, left_columns=None, right_columns=None, tqdm=_tqdm, pbar=None):
@@ -669,7 +662,7 @@ class Table(BaseTable):
         with each row from the second table
         """
         return joins.cross_join(
-            self, other, left_keys, right_keys, left_columns=None, right_columns=None, tqdm=_tqdm, pbar=None
+            self, other, left_keys, right_keys, left_columns, right_columns, tqdm=_tqdm, pbar=None
         )
 
     def lookup(self, other, *criteria, all=True, tqdm=_tqdm):  # TODO: This is single core code.
@@ -690,7 +683,7 @@ class Table(BaseTable):
               ('text 1', f, 'text 2')
               value from column 'text 1' is compared with value from column 'text 2'
         """
-        return lookups.lookup(self, other, *criteria, all=True, tqdm=_tqdm)
+        return lookups.lookup(self, other, *criteria, all, tqdm=_tqdm)
 
     def replace_missing_values(self, *args, **kwargs):
         raise AttributeError("See imputation")
@@ -742,7 +735,7 @@ class Table(BaseTable):
         Returns:
             table: table with replaced values.
         """
-        return imputation.imputation(self, targets, missing=None, method="carry forward", sources=None, tqdm=_tqdm)
+        return imputation.imputation(self, targets, missing, method, sources, tqdm=_tqdm)
 
     def transpose(self, tqdm=_tqdm):
         return pivots.transpose(self, tqdm)
@@ -779,7 +772,7 @@ class Table(BaseTable):
         |1244| 2445| 4456| mon      |     7|
 
         """
-        return pivots.pivot_transpose(self, columns, keep=None, column_name="transpose", value_name="value", tqdm=_tqdm)
+        return pivots.pivot_transpose(self, columns, keep, column_name, value_name, tqdm=_tqdm)
 
     def diff(self, other, columns=None):
         """compares table self with table other
@@ -797,197 +790,3 @@ class Table(BaseTable):
 
 # -------------- MULTI PROCESSING TASKS -----------------
 
-
-def not_in(a, b):
-    return not operator.contains(str(a), str(b))
-
-
-def _in(a, b):
-    """
-    enables filter function 'in'
-    """
-    return str(a) in str(b)
-    return operator.contains(str(a), str(b))  # TODO : check which method is faster
-
-
-lookup_ops = {
-    "in": _in,
-    "not in": not_in,
-    "<": operator.lt,
-    "<=": operator.le,
-    ">": operator.gt,
-    ">=": operator.ge,
-    "!=": operator.ne,
-    "==": operator.eq,
-}
-
-
-filter_ops = {
-    ">": operator.gt,
-    ">=": operator.ge,
-    "==": operator.eq,
-    "<": operator.lt,
-    "<=": operator.le,
-    "!=": operator.ne,
-    "in": _in,
-}
-
-filter_ops_from_text = {"gt": ">", "gteq": ">=", "eq": "==", "lt": "<", "lteq": "<=", "neq": "!=", "in": _in}
-
-
-def filter_evaluation_task(table_key, expression, shm_name, shm_index, shm_shape, slice_):
-    """
-    multiprocessing tasks for evaluating Table.filter
-    """
-    assert isinstance(table_key, str)  # 10 --> group = '/table/10'
-    assert isinstance(expression, dict)
-    assert len(expression) == 3
-    assert isinstance(shm_name, str)
-    assert isinstance(shm_index, int)
-    assert isinstance(shm_shape, tuple)
-    assert isinstance(slice_, slice)
-    c1 = expression.get("column1", None)
-    c2 = expression.get("column2", None)
-    c = expression.get("criteria", None)
-    assert c in filter_ops
-    f = filter_ops.get(c)
-    assert callable(f)
-    v1 = expression.get("value1", None)
-    v2 = expression.get("value2", None)
-
-    columns = mem.mp_get_columns(table_key)
-    if c1 is not None:
-        column_key = columns[c1]
-        dset_A = mem.get_data(f"/column/{column_key}", slice_)
-    else:  # v1 is active:
-        dset_A = np.array([v1] * (slice_.stop - slice_.start))
-
-    if c2 is not None:
-        column_key = columns[c2]
-        dset_B = mem.get_data(f"/column/{column_key}", slice_)
-    else:  # v2 is active:
-        dset_B = np.array([v2] * (slice_.stop - slice_.start))
-
-    existing_shm = shared_memory.SharedMemory(name=shm_name)  # connect
-    result_array = np.ndarray(shm_shape, dtype=np.bool, buffer=existing_shm.buf)
-    result_array[shm_index][slice_] = np.array([f(a, b) for a, b in zip(dset_A, dset_B)])  # Evaluate
-    existing_shm.close()  # disconnect
-
-
-def filter_merge_task(table_key, true_key, false_key, shm_name, shm_shape, slice_, filter_type):
-    """
-    multiprocessing task for merging data after the filter task has been completed.
-    """
-    existing_shm = shared_memory.SharedMemory(name=shm_name)  # connect
-    result_array = np.ndarray(shm_shape, dtype=np.bool, buffer=existing_shm.buf)
-    mask_source = result_array
-
-    if filter_type == "any":
-        true_mask = np.any(mask_source, axis=0)
-    else:
-        true_mask = np.all(mask_source, axis=0)
-    true_mask = true_mask[slice_]
-    false_mask = np.invert(true_mask)
-
-    # 2. load source
-    columns = mem.mp_get_columns(table_key)
-
-    true_columns, false_columns = {}, {}
-    for col_name, column_key in columns.items():
-        col = Column(key=column_key)
-        slize = col.get_numpy(slice_)
-        true_values = slize[true_mask]
-        if np.any(true_mask):
-            true_columns[col_name] = mem.mp_write_column(true_values)
-        false_values = slize[false_mask]
-        if np.any(false_mask):
-            false_columns[col_name] = mem.mp_write_column(false_values)
-
-    mem.mp_write_table(true_key, true_columns)
-    mem.mp_write_table(false_key, false_columns)
-
-    existing_shm.close()  # disconnect
-
-
-def indexing_task(
-    source_key, destination_key, shm_name_for_sort_index, shape, slice_=slice(None), shm_name_for_sort_index_mask=None
-):
-    """
-    performs the creation of a column sorted by sort_index (shared memory object).
-    source_key: column to read
-    destination_key: column to write
-    shm_name_for_sort_index: sort index' shm.name created by main.
-    shm_name_for_sort_index_mask: sort index' shm.name created by main containing None mask.
-    shape: shm array shape.
-
-    *used by sort and all join functions.
-    """
-    existing_shm = shared_memory.SharedMemory(name=shm_name_for_sort_index)  # connect
-    sort_index = np.ndarray(shape, dtype=np.int64, buffer=existing_shm.buf)
-
-    sort_slice = sort_index[slice_]
-
-    if shm_name_for_sort_index_mask is not None:
-        existing_mask_shm = shared_memory.SharedMemory(name=shm_name_for_sort_index_mask)  # connect
-        sort_index_mask = np.ndarray(shape, dtype=np.int8, buffer=existing_mask_shm.buf)
-    else:
-        sort_index_mask = None
-
-    data = mem.get_data(f"/column/{source_key}", slice(None))  # --- READ!
-
-    values = [None] * len(sort_slice)
-
-    start_offset = 0 if slice_.start is None else slice_.start
-
-    for i, (j, ix) in enumerate(enumerate(sort_slice, start_offset)):
-        if sort_index_mask is not None and sort_index_mask[j] == 1:
-            values[i] = None
-        else:
-            values[i] = data[ix]
-
-    existing_shm.close()  # disconnect
-    if sort_index_mask is not None:
-        existing_mask_shm.close()
-    mem.mp_write_column(values, column_key=destination_key)  # --- WRITE!
-
-
-def compress_task(source_key, destination_key, shm_index_name, shape):
-    """
-    compresses the source using boolean mask from shared memory
-
-    source_key: column to read
-    destination_key: column to write
-    shm_name_for_sort_index: sort index' shm.name created by main.
-    shape: shm array shape.
-    """
-    existing_shm = shared_memory.SharedMemory(name=shm_index_name)  # connect
-    mask = np.ndarray(shape, dtype=np.int64, buffer=existing_shm.buf)
-
-    data = mem.get_data(f"/column/{source_key}", slice(None))  # --- READ!
-    values = np.compress(mask, data)
-
-    existing_shm.close()  # disconnect
-    mem.mp_write_column(values, column_key=destination_key)  # --- WRITE!
-
-
-def _maskify(arr):
-    none_mask = [False] * len(arr)  # Setting the default
-
-    for i in range(len(arr)):
-        if arr[i] is None:  # Check if our value is None
-            none_mask[i] = True
-            arr[i] = 0  # Remove None from the original array
-
-    return none_mask
-
-
-def _share_mem(inp_arr, dtype):
-    len_ = len(inp_arr)
-    size = np.dtype(dtype).itemsize * len_
-    shape = (len_,)
-
-    out_shm = shared_memory.SharedMemory(create=True, size=size)  # the co_processors will read this.
-    out_arr_index = np.ndarray(shape, dtype=dtype, buffer=out_shm.buf)
-    out_arr_index[:] = inp_arr
-
-    return out_arr_index, out_shm
