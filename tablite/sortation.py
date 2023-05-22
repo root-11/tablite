febb90d7179f927f45300cd6e7b23ca3f5dc41d0
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2023-05-22 21:56:22 +0100

    reworked sortation to work with fixed memory footprint.

--- tablite/sortation.py
+++ tablite/sortation.py
@@ -1,34 +1,47 @@
-def sort_index(self, sort_mode="excel", tqdm=_tqdm, pbar=None, **kwargs):
+import os
+from config import Config
+import numpy as np
+import psutil
+from mplite import Task, TaskManager
+from mp_utils import shared_memory, reindex_task
+from sort_utils import modes as sort_modes
+from sort_utils import rank as sort_rank
+from base import Table, Column, Page
+from utils import sub_cls_check, type_check
+
+
+def sort_index(T, sort_mode="excel", tqdm=_tqdm, pbar=None, **kwargs):
     """
     helper for methods `sort` and `is_sorted`
 
     param: sort_mode: str: "alphanumeric", "unix", or, "excel" (default)
     param: **kwargs: sort criteria. See Table.sort()
     """
-    logging.info("Table.sort_index running 1 core")  # This is single core code.
+
+    sub_cls_check(T, Table)
 
     if not isinstance(kwargs, dict):
         raise ValueError("Expected keyword arguments, did you forget the ** in front of your dict?")
     if not kwargs:
-        kwargs = {c: False for c in self.columns}
+        kwargs = {c: False for c in T.columns}
 
     for k, v in kwargs.items():
-        if k not in self.columns:
+        if k not in T.columns:
             raise ValueError(f"no column {k}")
         if not isinstance(v, bool):
             raise ValueError(f"{k} was mapped to {v} - a non-boolean")
 
-    if sort_mode not in sortation.modes:
-        raise ValueError(f"{sort_mode} not in list of sort_modes: {list(sortation.Sortable.modes.modes)}")
+    if sort_mode not in sort_modes:
+        raise ValueError(f"{sort_mode} not in list of sort_modes: {list(sort_modes)}")
 
-    rank = {i: tuple() for i in range(len(self))}  # create index and empty tuple for sortation.
+    rank = {i: tuple() for i in range(len(T))}  # create index and empty tuple for sortation.
 
     _pbar = tqdm(total=len(kwargs.items()), desc="creating sort index") if pbar is None else pbar
 
     for key, reverse in kwargs.items():
-        col = self._columns[key][:]
+        col = T[key][:]
         col = col.tolist() if isinstance(col, np.ndarray) else col
-        ranks = sortation.rank(values=set(col), reverse=reverse, mode=sort_mode)
+        ranks = sort_rank(values=set(col), reverse=reverse, mode=sort_mode)
         assert isinstance(ranks, dict)
         for ix, v in enumerate(col):
             rank[ix] += (ranks[v],)  # add tuple
@@ -40,10 +53,10 @@ def sort_index(self, sort_mode="excel", tqdm=_tqdm, pbar=None, **kwargs):
     new_order.sort()
     sorted_index = [i for _, i in new_order]  # new index is extracted.
     new_order.clear()
-    return sorted_index
+    return np.array(sorted_index)
 
 
-def reindex(self, index):
+def reindex(T, index):
     """
     index: list of integers that declare sort order.
 
@@ -58,61 +71,69 @@ def reindex(self, index):
         result: ['a','c','e','g','b','d','f','h']
 
     """
-    if index is not None:
-        if not isinstance(index, list):
-            raise TypeError
-        if max(index) >= len(self):
-            raise IndexError("index out of range: max(index) > len(self)")
-        if min(index) < -len(self):
-            raise IndexError("index out of range: min(index) < -len(self)")
-        if not all(isinstance(i, int) for i in index):
-            raise TypeError
+    sub_cls_check(T, Table)
+    if isinstance(index, list):
+        index = np.ndarray(index, dtype=np.int64)
+    type_check(index, np.ndarray)
+    if max(index) >= len(T):
+        raise IndexError("index out of range: max(index) > len(self)")
+    if min(index) < -len(T):
+        raise IndexError("index out of range: min(index) < -len(self)")
 
-    if (
-        len(self) * len(self.columns) < Config.SINGLE_PROCESSING_LIMIT
-    ):  # the task is so small that multiprocessing doesn't make sense.
-        t = Table()
-        for col_name, col in self._columns.items():  # this LOOP can be done with TaskManager
-            data = list(col[:])
-            t.add_column(col_name, data=[data[ix] for ix in index])
-        return t
-
+    cpus = max(psutil.cpu_count(), 1)
+    if cpus < 2 or Config.MULTIPROCESSING_MODE == Config.FALSE:
+        return _sp_reindex(T, index)
     else:
-        arr = np.zeros(shape=(len(index),), dtype=np.int64)
-        shm = shared_memory.SharedMemory(create=True, size=arr.nbytes)  # the co_processors will read this.
-        sort_index = np.ndarray(arr.shape, dtype=arr.dtype, buffer=shm.buf)
-        sort_index[:] = index
-
-        tasks = []
-        columns_refs = {}
-        for name in self.columns:
-            col = self[name]
-            columns_refs[name] = d_key = mem.new_id("/column")
-            tasks.append(
-                Task(
-                    indexing_task,
-                    source_key=col.key,
-                    destination_key=d_key,
-                    shm_name_for_sort_index=shm.name,
-                    shape=arr.shape,
-                )
-            )
-
-        with TaskManager(cpu_count=min(psutil.cpu_count(), len(tasks))) as tm:
-            errs = tm.execute(tasks)
-            if any(errs):
-                raise Exception("\n".join(filter(lambda x: x is not None, errs)))
-
-        table_key = mem.new_id("/table")
-        mem.create_table(key=table_key, columns=columns_refs)
-
-        shm.close()
-        shm.unlink()
-        t = Table.load(path=mem.path, key=table_key)
-        return t
+        return _mp_reindex(T, index)
 
 
-def sort(self, sort_mode="excel", **kwargs):
+def _sp_reindex(T, index):
+    t = type(T)()
+    for name in T.columns:
+        t[name] = np.take(T[name][:], index)
+        np.tak
+    return t
+
+
+def _mp_reindex(T, index):
+    shm = shared_memory.SharedMemory(create=True, size=index.nbytes)  # the co_processors will read this.
+    sort_index = np.ndarray(index.shape, dtype=index.dtype, buffer=shm.buf)
+    sort_index[:] = index
+
+    new = {}
+    tasks = []
+    for name in T.columns:
+        col = T[name]
+        new[name] = []
+
+        start, end = 0, 0
+        for page in col.pages:
+            start, end = end, start + len(page)
+            src = page.path
+            dst = page.path.parent / f"{next(Page.ids)}.npy"
+            t = Task(reindex_task, src, dst, shm, start, end)
+            new.append(dst)
+
+    cpus = psutil.cpu_count()
+    with TaskManager(cpu_count=cpus) as tm:
+        errs = tm.execute(tasks)
+        if any(errs):
+            raise Exception("\n".join(filter(lambda x: x is not None, errs)))
+
+    shm.close()
+    shm.unlink()
+
+    t = type(T)()
+    for name in T.columns:
+        t[name] = Column(t.path)
+        for dst in new[name]:
+            data = np.load(dst, allow_pickle=True, fix_imports=False)
+            t[name].extend(data)
+            os.remove(dst)
+    return t
+
+
+def sort(T, sort_mode="excel", **kwargs):
     """Perform multi-pass sorting with precedence given order of column names.
     sort_mode: str: "alphanumeric", "unix", or, "excel"
     kwargs:
@@ -124,58 +145,22 @@ def sort(self, sort_mode="excel", **kwargs):
     Table.sort('A'=True, 'B'=False) means sort 'A' in descending order, then (2nd priority)
     sort B in ascending order.
     """
-    if (
-        len(self) * len(self.columns) < Config.SINGLE_PROCESSING_LIMIT
-    ):  # the task is so small that multiprocessing doesn't make sense.
-        sorted_index = self.sort_index(sort_mode=sort_mode, **kwargs)
+    sub_cls_check(T, Table)
 
-        t = Table()
-        for col_name, col in self._columns.items():
-            data = list(col[:])
-            t.add_column(col_name, data=[data[ix] for ix in sorted_index])
-        return t
+    index = sort_index(sort_mode=sort_mode, **kwargs)
+
+    cpus = max(psutil.cpu_count(), 1)
+    if cpus < 2 or Config.MULTIPROCESSING_MODE == Config.FALSE:
+        return _sp_reindex(T, index)
     else:
-        arr = np.zeros(shape=(len(self),), dtype=np.int64)
-        shm = shared_memory.SharedMemory(create=True, size=arr.nbytes)  # the co_processors will read this.
-        sort_index = np.ndarray(arr.shape, dtype=arr.dtype, buffer=shm.buf)
-        sort_index[:] = self.sort_index(sort_mode=sort_mode, **kwargs)
-
-        tasks = []
-        columns_refs = {}
-        for name in self.columns:
-            col = self[name]
-            columns_refs[name] = d_key = mem.new_id("/column")
-            tasks.append(
-                Task(
-                    indexing_task,
-                    source_key=col.key,
-                    destination_key=d_key,
-                    shm_name_for_sort_index=shm.name,
-                    shape=arr.shape,
-                )
-            )
-
-        with TaskManager(cpu_count=min(psutil.cpu_count(), len(tasks))) as tm:
-            errs = tm.execute(tasks)
-            if any(errs):
-                raise Exception("\n".join(filter(lambda x: x is not None, errs)))
-
-        table_key = mem.new_id("/table")
-        mem.create_table(key=table_key, columns=columns_refs)
-
-        shm.close()
-        shm.unlink()
-        t = type(self).load(path=mem.path, key=table_key)
-        return t
+        return _mp_reindex(T, index)
 
 
-def is_sorted(self, **kwargs):
+def is_sorted(T, **kwargs):
     """Performs multi-pass sorting check with precedence given order of column names.
     **kwargs: optional: sort criteria. See Table.sort()
     :return bool
     """
-    logging.info("Table.is_sorted running 1 core")  # TODO: This is single core code.
-    sorted_index = self.sort_index(**kwargs)
-    if any(ix != i for ix, i in enumerate(sorted_index)):
-        return False
-    return True
+    index = sort_index(**kwargs)
+    match = np.arange(len(T))
+    return np.all(index == match)
