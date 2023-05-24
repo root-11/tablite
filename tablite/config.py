8433a91244ef2eb4d2c8456a8e609ec51fb45362
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2023-05-24 15:14:22 +0100

    Added monitoring decorator for Column.__setitem__ to detect page fragmentation. I'm not sure this is always a good idea.

--- tablite/config.py
+++ tablite/config.py
@@ -29,12 +29,24 @@ class Config(object):
     when the number of fields (rows x columns) exceed this value,
     multiprocessing is used.
     """
+
     workdir = pathlib.Path(os.environ.get("TABLITE_TMPDIR", f"{tempfile.gettempdir()}/tablite-tmp"))
     workdir.mkdir(parents=True, exist_ok=True)
 
     PAGE_SIZE = 1_000_000  # sets the page size limit.
+
+    PAGE_FRAGMENTATION_CHECK = False
+    PAGE_FRAGMENTATION_TOLERANCE = 2
+    # at 2 pages per 1M fields, pages will be consolidated.
+    PAGE_FRAGMENTATION_INSPECTION_RATE = 50
+    # After 50 inserts/updates in a column fragmentation
+    # a check is performed. If the number of pages exceed
+    # the fragmentation tolerance, the data will be are
+    # repaginated to PAGE_SIZE.
+
     ENCODING = "UTF-8"  # sets the page encoding when using bytes
-    DISK_LIMIT = 10e9  # 10e9 (10Gb) on 100 Gb disk means raise at 90 Gb disk usage.
+    DISK_LIMIT = 10e9  # 10e9 (10Gb) on 100 Gb disk means raise at
+    # 90 Gb disk usage.
 
     SINGLE_PROCESSING_LIMIT = 1_000_000
     # when the number of fields (rows x columns)
@@ -57,9 +69,31 @@ class Config(object):
 
     @classmethod
     def reset(cls):
-        """ Resets the config class to original values."""
+        """Resets the config class to original values."""
         for k, v in _default_values.items():
             setattr(Config, k, v)
 
 
 _default_values = {k: v for k, v in Config.__dict__.items() if not k.startswith("__") or callable(v)}
+
+
+class FragmentationMonitor(object):  # TODO: To be added to Column.__setitem__(...)
+    """decorator to monitor for page fragmentation."""
+    def __init__(self, arg) -> None:
+        self._arg = arg
+        self._change_counter = 0
+
+    def __call__(self, *args: Any) -> Any:
+        result = self._arg(*args)
+        if Config.PAGE_FRAGMENTATION_CHECK:
+            self._change_counter += 1
+            if self._change_counter >= Config.PAGE_FRAGMENTATION_INSPECTION_RATE:
+                self._change_counter = 0
+
+                column = args[0]
+                assert isinstance(column, Column)
+                n_pages = math.ceil(len(column) / Config.PAGE_SIZE)
+                if len(column.pages) / n_pages > Config.PAGE_FRAGMENTATION_TOLERANCE:
+                    column.repaginate()
+
+        return result
