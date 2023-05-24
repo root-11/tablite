203d4e38b8aa51c2ad72b1b05afeb695ed10171a
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2023-05-24 13:29:01 +0100

    Moved test of datasets to /tests

--- tablite/datasets.py
+++ tablite/datasets.py
@@ -1,7 +1,8 @@
 import random
 from datetime import datetime
 from string import ascii_uppercase
-from tablite.base import Table
+from tablite import Table
+from tablite.base import Table as BaseTable
 
 
 def synthetic_order_data(rows=100_000):
@@ -41,6 +42,8 @@ def synthetic_order_data(rows=100_000):
     L2 = ["ABC", "XYZ", ""]
 
     t = Table()
+    assert isinstance(t, Table)
+    assert not isinstance(t, BaseTable)
     t["#"] = list(range(1, rows + 1))
     # 1 - mock orderid
     t["1"] = [random.randint(18_778_628_504, 2277_772_117_504) for i in range(1, rows + 1)]
@@ -68,87 +71,3 @@ def synthetic_order_data(rows=100_000):
     return t
 
 
-if __name__ == "__main__":
-    import logging
-
-    log = logging.getLogger(__name__)
-    log.setLevel(logging.DEBUG)
-    console = logging.StreamHandler()
-    console.setLevel(level=logging.DEBUG)
-    formatter = logging.Formatter("%(levelname)s : %(message)s")
-    console.setFormatter(formatter)
-    log.addHandler(console)
-    import time
-    import os
-    import gc
-    import psutil
-    from pathlib import Path
-    from tempfile import tempdir
-
-    process = psutil.Process(os.getpid())
-    baseline_memory = process.memory_info().rss
-
-    length = 2_000_000
-    fn = Path(tempdir) / "synth data.tpz"
-    start = time.process_time()
-    t = synthetic_order_data(length)
-    created = time.process_time()
-    while gc.collect():
-        pass
-
-    memory_after_table_creation = process.memory_info().rss
-    memory_for_t = memory_after_table_creation - baseline_memory
-
-    print(f"creating {t} took {created-start} sec")
-    print(f"current RAM for table: {memory_for_t//1_000_000}Mb")
-    print(t.show())
-
-    t.save(fn)
-    saved = time.process_time()
-    print(f"saving {t} took {saved-created} sec  {os.path.getsize(fn)//1000:,}kb")
-
-    t2 = t.load(fn)
-    loaded = time.process_time()
-    print(f"loading {t} took {loaded-saved} sec")
-
-    del t2
-    while gc.collect():
-        pass
-    os.remove(fn)
-
-    start = time.process_time()
-    t3 = t.copy()
-    end = time.process_time()
-    print(f"t.copy took {end-start} sec")
-    del t3
-
-    start = time.process_time()
-    assert 1 in t["#"]
-    short = time.process_time()
-    assert length in t["#"]
-    long = time.process_time()
-    print(f"t.__contains__ took {short-start} / {long-short} (best-/worst case) sec")
-
-    start = time.process_time()
-    d = t.types()
-    end = time.process_time()
-    print(f"t.types() took {end-start} sec")
-
-    for name in t.columns:
-        start = time.process_time()
-        L = t[name].unique()
-        end = time.process_time()
-        print(f"t.unique({name}) took {end-start} sec.")
-
-    for name in t.columns:
-        start = time.process_time()
-        d = t.index(name)
-        end = time.process_time()
-        print(f"t.index({name}) took {end-start} sec.")
-
-    names = ["4", "7", "8", "9"]
-    for ix in range(1, 5):
-        start = time.process_time()
-        t.index(*names[:ix])
-        end = time.process_time()
-        print(f"t.index({names[:ix]}) took {end-start}")
