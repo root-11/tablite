b9eb4ce53c0eb4ad1248b37604b93974eb13f44f
Ratchet <audryzas@hotmail.com>
2023-09-25 10:31:44 +0300

    * fixed some stuff

--- _nimlite/nimlite.nim
+++ _nimlite/nimlite.nim
@@ -1,7 +1,40 @@
-import nimpy
-import std/[options, strutils]
+import std/[os, options, strutils, osproc, sugar]
 import encfile, table, csvparse, textreader, utils, pylayer, taskargs
 
+proc saveTasks(task: TabliteTasks, pid: string): string =
+    let task_path = pid & "/pages/tasks.txt"
+    let fh = open(task_path, fmWrite)
+
+    for column_task in task.tasks:
+        fh.write("\"" & getAppFilename() & "\" ")
+
+        fh.write("--encoding=" & task.encoding & " ")
+        fh.write("--guess_dtypes=" & $task.guess_dtypes & " ")
+
+        fh.write("--delimiter=\"" & task.dialect.delimiter & "\" ")
+        fh.write("--quotechar=\"" & task.dialect.quotechar & "\" ")
+        fh.write("--escapechar=\"" & task.dialect.escapechar & "\" ")
+        fh.write("--lineterminator=\"" & task.dialect.lineterminator & "\" ")
+        fh.write("--doublequote=" & $task.dialect.doublequote & " ")
+        fh.write("--skipinitialspace=" & $task.dialect.skipinitialspace & " ")
+        fh.write("--quoting=" & task.dialect.quoting & " ")
+
+        fh.write("task ")
+
+        fh.write("--pages=\"" & column_task.pages.join(",") & "\" ")
+        fh.write("--fields=\"" & task.import_fields.join(",") & "\" ")
+
+        fh.write("\"" & task.path & "\" ")
+        fh.write($column_task.offset & " ")
+        fh.write(task.page_size)
+
+        fh.write("\n")
+
+    fh.close()
+
+    return task_path
+
+import nimpy
 # include pylib
 proc text_reader_task(
     path: string,
@@ -67,7 +100,7 @@ proc text_reader(
         var arg_delimiter = (if delimiter.len == 1: delimiter[0] else: raise newException(Exception, "'delimiter' not a char"))
         var arg_text_qualifier = (if text_qualifier.len == 1: text_qualifier[0] else: raise newException(Exception, "'text_qualifier' not a char"))
 
-        return textReader(
+        let table = textReader(
                 pid = pid,
                 path = path,
                 encoding = arg_encoding,
@@ -84,6 +117,10 @@ proc text_reader(
                 page_size=page_size,
                 guess_dtypes=guess_dtypes
         )
+
+        discard table.task.saveTasks(pid)
+
+        return table
     except Exception as e:
         echo $e.msg & "\n" & $e.getStackTrace
         raise e
@@ -113,6 +150,20 @@ proc runTask(path: string, encoding: string, dialect: TabliteDialect, task: Tabl
         count=count
     ).textReaderTask
 
+proc executeParallel(path: string): void =
+    echo "Executing tasks: '" & path & "'"
+    let args = @[
+        "--progress",
+        "-a",
+        "\"" & path & "\""
+    ]
+
+    let para = "/usr/bin/parallel"
+    let ret_code = execCmd(para & " " & args.join(" "))
+
+    if ret_code != 0:
+        raise newException(Exception, "Process failed with errcode: " & $ret_code)
+
 when isMainModule and appType != "lib":
     import argparse
     import std/times
@@ -134,8 +185,15 @@ when isMainModule and appType != "lib":
         option(
             "-e", "--encoding",
             help="file encoding",
-            choices = @["UTF8", "UTF16"],
-            default=some("UTF8")
+            choices = @["ENC_UTF8", "ENC_UTF16"],
+            default=some("ENC_UTF8")
+        )
+
+        option(
+            "-dt", "--guess_dtypes",
+            help="gues datatypes",
+            choices = @boolean_choices,
+            default=some("true")
         )
 
         option("--delimiter", help="text delimiter", default=some(","))
@@ -176,17 +234,16 @@ when isMainModule and appType != "lib":
             arg("execute", help="execute immediatly")
             arg("multiprocess", help="use multiprocessing")
             run:
-                discard
+                path_csv = opts.path
         command("task"):
             option("--pages", help="task pages", required = true)
-            option("--fields_keys", help="field keys", required = true)
-            option("--fields_vals", help="field vals", required = true)
+            option("--fields", help="fields to import", required = true)
 
             arg("path", help="file path")
             arg("offset", help="file offset")
             arg("count", help="line count")
             run:
-                discard
+                path_csv = opts.path
         run:
             var delimiter = opts.delimiter.unescapeSeq()
             var quotechar = opts.quotechar.unescapeSeq()
@@ -225,16 +282,17 @@ when isMainModule and appType != "lib":
             )
 
             case opts.encoding.toUpper():
-                of "UTF8": encoding = ENC_UTF8
-                of "UTF16": encoding = ENC_UTF16
+                of "ENC_UTF8": encoding = ENC_UTF8
+                of "ENC_UTF16": encoding = ENC_UTF16
                 else: raise newException(Exception, "invalid 'encoding'")
+
+            guess_dtypes = opts.guess_dtypes in boolean_true_choices
 
     let opts = p.parse()
     p.run()
 
     if opts.import.isNone and opts.task.isNone:
         guess_dtypes = true
-
         # (path_csv, encoding) = ("/home/ratchet/Documents/dematic/tablite/tests/data/bad_empty.csv", ENC_UTF8)
         # (path_csv, encoding) = ("/home/ratchet/Documents/dematic/tablite/tests/data/gdocs1.csv", ENC_UTF8)
         (path_csv, encoding) = ("/home/ratchet/Documents/dematic/callisto/tests/testing/data/Dematic YDC Order Data.csv", ENC_UTF8)
@@ -244,20 +302,55 @@ when isMainModule and appType != "lib":
         # (path_csv, encoding) = ("/home/ratchet/Documents/dematic/tablite/tests/data/utf16_be.csv", ENC_UTF16)
         # (path_csv, encoding) = ("/home/ratchet/Documents/dematic/tablite/tests/data/utf16_le.csv", ENC_UTF16)
 
+        let multiprocess = true
+        let execute = true
         let d0 = getTime()
 
         let table = importTextFile(pid, path_csv, encoding, dialect, cols, page_size, guess_dtypes)
         let task = table.task
 
-        for column_task in task.tasks:
-            runTask(task.path, task.encoding, task.dialect, column_task, task.import_fields, int task.page_size, task.guess_dtypes)
-            break
-        
+        if multiprocess:
+            let task_path = task.saveTasks(pid)
+
+            if execute:
+                executeParallel(task_path)
+        else:
+            for column_task in task.tasks:
+                runTask(task.path, task.encoding, task.dialect, column_task, task.import_fields, int task.page_size, task.guess_dtypes)
+
         let d1 = getTime()
         echo $(d1 - d0)
 
     else:
-        raise newException(Exception, "not implemented")
+        if opts.import.isSome:
+            raise newException(Exception, "not implemented: 'import'")
+        elif opts.task.isSome:
+            let tdia = newTabliteDialect(dialect)
+            let ttask = newTabliteTask(opts.task.get.pages.split(","), uint parseInt(opts.task.get.offset))
+            let fields = collect: (for k in opts.task.get.fields.split(","): uint parseInt(k))
+            let count = parseInt(opts.task.get.count)
+
+            runTask(path_csv, $encoding, tdia, ttask, fields, count, guess_dtypes)
+
+            # let fields = opts.task.get.fields.split(",")
+            # let pages = 
+            # toTaskArgs(
+            #     path=path_csv,
+            #     encoding= $encoding,
+            #     dia_delimiter= $dialect.delimiter,
+            #     dia_quotechar= $dialect.quotechar,
+            #     dia_escapechar= $dialect.escapechar,
+            #     dia_doublequote=dialect.doublequote,
+            #     dia_quoting= $dialect.quoting,
+            #     dia_skipinitialspace=dialect.skipinitialspace,
+            #     dia_lineterminator= $dialect.lineterminator,
+            #     dia_strict=dialect.strict,
+            #     guess_dtypes=guess_dtypes,
+            #     tsk_pages=opts.task.get.pages.split(","),
+            #     tsk_offset=uint parseInt(opts.task.get.offset),
+            #     import_fields=pages,
+            #     count=parseInt(opts.task.get.count)
+            # ).textReaderTask
 
 # import std/[os, enumerate, sugar, times, tables, sequtils, json, unicode, osproc, options]
 # import argparse
