a82a13bd745ea39f90896eb5bb103698720cdd44
root-11 <4048422+root-11@users.noreply.github.com>
2023-12-08 17:00:54 +0000

    updated test to account for *=

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -9,4 +9,11 @@ def test01():
     t = synthetic_order_data(length)
     assert len(t) == length
     assert t["#"] == list(range(1, length + 1))
+
+    a = t["#"].types()
+    t *= 3
+    assert len(t) == length * 3
+    b = t["#"].types()
+    assert {k: v * 3 for k, v in a.items()} == b
+
     Config.PAGE_SIZE = old_page_size
