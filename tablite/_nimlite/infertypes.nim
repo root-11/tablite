44241813d8707cebdd9e1a7e4cdbefb49f7cffd5
Ratchet <audryzas@hotmail.com>
2023-12-22 14:24:07 +0200

    date and datetime pages

--- tablite/_nimlite/infertypes.nim
+++ tablite/_nimlite/infertypes.nim
@@ -1,9 +1,7 @@
-from std/math import floor, pow
+from std/math import pow
 from std/unicode import runeLen
 import std/strutils
-import pickling
-
-const DAYS_IN_MONTH = [-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+import pickling, dateutils
 
 type ParseShortDate = enum
     DONT_CARE
@@ -48,10 +46,10 @@ proc inferInt*(str: ptr string, is_simple: bool, is_american: bool): int {.inlin
         if first_char == '+' or first_char == '-':
             sign = (if first_char == '+': 1 else: -1)
             first_char = sstr[1]
-            
+
             if not first_char.isDigit():
                 raise newException(ValueError, "NaN")
-            
+
             inc ch_offset
         else:
             raise newException(ValueError, "NaN")
@@ -112,10 +110,10 @@ proc inferFloat*(str: ptr string, is_simple: bool, is_american: bool): float {.i
         if first_char == '+' or first_char == '-':
             sign = (if first_char == '+': 1.0 else: -1.0)
             first_char = sstr[1]
-            
+
             if not first_char.isDigit():
                 raise newException(ValueError, "NaN")
-            
+
             inc ch_offset
 
         if first_char == '.' and is_american or first_char == ',' and not is_american:
@@ -159,7 +157,7 @@ proc inferFloat*(str: ptr string, is_simple: bool, is_american: bool): float {.i
         raise newException(ValueError, "NaN")
 
     var exp: float = (if is_exponent: pow(10, (exponent * exponent_sign)) else: 1.0)
-    
+
     return (value * sign) * exp
 
 proc parseDateWords(str: ptr string, is_short: ParseShortDate, allow_time: bool): (array[3, string], int) {.inline.} =
@@ -246,12 +244,6 @@ proc parseDateWords(str: ptr string, is_short: ParseShortDate, allow_time: bool)
 
     return (substrings, 8)
 
-proc isLeapYear(year: int): bool {.inline.} = year mod 4 == 0 and (year mod 100 != 0 or year mod 400 == 0)
-proc getDaysInMonth(year, month: int): int {.inline.} =
-    if month == 2 and isLeapYear(year):
-        return 29
-    return DAYS_IN_MONTH[month]
-
 proc guessDate(date_words: ptr array[3, string], is_american: bool): (int, int, int) {.inline.} =
     var year, month, day: int
     var month_or_day: array[2, int]
@@ -328,49 +320,7 @@ proc inferDate*(str: ptr string, is_short: bool, is_american: bool): PY_Date {.i
 
     return wordsToDate(date_words.unsafeAddr, is_american)
 
-proc divmod(x: int, y: int): (int, int) {.inline.} =
-    let z = int(floor(x / y))
 
-    return (z, x - y * z)
-
-proc toTimedelta(
-    weeks = 0, days = 0, hours = 0, minutes = 0, seconds = 0, milliseconds = 0, microseconds: int = 0
-): (int, int, int) {.inline.} =
-    var d, s, us: int
-
-    var v_weeks = weeks
-    var v_days = days
-    var v_hours = hours
-    var v_minutes = minutes
-    var v_seconds = seconds
-    var v_milliseconds = milliseconds
-    var v_microseconds = microseconds
-
-    # Normalize everything to days, seconds, microseconds.
-    v_days += v_weeks*7
-    v_seconds += v_minutes*60 + v_hours*3600
-    v_microseconds += v_milliseconds*1000
-
-    d = v_days
-
-    (v_days, v_seconds) = divmod(v_seconds, 24*3600)
-
-    d += v_days
-    s += int(v_seconds) # can't overflow
-
-    v_microseconds = int(v_microseconds)
-    (v_seconds, v_microseconds) = divmod(v_microseconds, 1000000)
-    (v_days, v_seconds) = divmod(v_seconds, 24*3600)
-    d += v_days
-    s += v_seconds
-
-    # Just a little bit of carrying possible for microseconds and seconds.
-    (v_seconds, us) = divmod(v_microseconds, 1000000)
-    s += v_seconds
-    (v_days, s) = divmod(s, 24*3600)
-    d += v_days
-
-    return (d, s, us)
 
 proc parse_hh_mm_ss_ff(tstr: ptr string): (uint8, uint8, uint8, uint32) {.inline.} =
     # Parses things of the form HH[:MM[:SS[.fff[fff]]]]
--- tablite/_nimlite/utils.nim
+++ tablite/_nimlite/utils.nim
@@ -1,3 +1,5 @@
+from std/math import floor
+
 proc uniqueName*(desired_name: string, name_list: seq[string]): string {.inline.} =
     var name = desired_name
     var idx = 1
@@ -16,3 +18,17 @@ proc unescapeSeq*(str: string): string {.inline.} = # nim has no true unescape
         of "\\\\": return "\\"
 
     return str
+
+proc divmod*(x: int, y: int): (int, int) {.inline.} =
+    let z = int(floor(x / y))
+
+    return (z, x - y * z)
+
+proc extractUnit*(d: int, unit: int): (int, int) {.inline.} =
+    var (idiv, imod) = divmod(d, unit)
+
+    if (imod < 0):
+        imod += unit
+        idiv -= 1
+
+    return (idiv, imod)
