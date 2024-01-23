c58c5aa65bcffddd5498886aabf958e64a230b4e
Ratchet <audryzas@homtail.com>
2024-01-23 10:28:03 +0200

    cleanup

--- tablite/_nimlite/column_selector.nim
+++ tablite/_nimlite/column_selector.nim
@@ -1,9 +1,8 @@
-import std/[os, tables, sugar, sets, sequtils, strutils, cpuinfo, paths, enumerate, unicode, times, macros]
+import std/[os, tables, sugar, sets, sequtils, strutils, paths, enumerate, unicode, times, macros]
 import nimpy as nimpy
 from nimpyext import `!`
 import utils
 import std/options as opt
-from std/math import clamp
 import pymodules as pymodules
 import pytypes
 import numpy
@@ -72,147 +71,133 @@ template makePage[T: typed](dt: typedesc[T], page: BaseNDArray, mask: var seq[Ma
     template createCastErrorReason(v: string, kind: KindObjectND): string =
         "Cannot cast '" & v & "' from '" & original_name & "[" & getTypeUserName(kind) & "]' to '" & desired_name & "[" & getTypeUserName(desired_type) & "]'."
 
-    template createNoneErrorReason(): string =
-        "'" & original_name & "' cannot be empty in '" & desired_name & "[" & getTypeUserName(desired_type) & "]'."
+    when page is ObjectNDArray:
+        template createNoneErrorReason(): string =
+            "'" & original_name & "' cannot be empty in '" & desired_name & "[" & getTypeUserName(desired_type) & "]'."
 
-    template createEmptyErrorReason(): string =
-        "'" & original_name & "' cannot be empty string in '" & desired_name & "[" & getTypeUserName(desired_type) & "]'."
+    when page is UnicodeNDArray or page is ObjectNDArray:
+        template createEmptyErrorReason(): string =
+            "'" & original_name & "' cannot be empty string in '" & desired_name & "[" & getTypeUserName(desired_type) & "]'."
 
-    echo ">>>page type" & T.name
+    when page is ObjectNDArray:
+        # this is an object page, check for strings and nones
+        var canBeNone = false
+        let types = page.getPageTypes()
 
+        if K_NONETYPE in types:
+            if likely(types[K_NONETYPE] > 0):
+                # there is at least one none in the page, we need to check for it
+                canBeNone = true
+
+        if not canBeNone and K_STRING in types:
+            if likely(types[K_STRING] > 0):
+                # there can be at least one empty string in the page, we need to check forit
+                canBeNone = true
+    else:
+        var inTypeKind {.noinit.}: KindObjectND
+
+        case page.kind:
+        of K_BOOLEAN: inTypeKind = KindObjectND.K_BOOLEAN
+        of K_INT8, K_INT16, K_INT32, K_INT64: inTypeKind = KindObjectND.K_INT
+        of K_FLOAT32, K_FLOAT64: inTypeKind = KindObjectND.K_FLOAT
+        of K_DATE: inTypeKind = KindObjectND.K_DATE
+        of K_DATETIME: inTypeKind = KindObjectND.K_DATETIME
+        of K_UNICODE: inTypeKind = KindObjectND.K_STRING
+        of K_OBJECT: discard # we're never this type
 
     when T is UnicodeNDArray:
-        var canBeNone: bool
-        var isObjectPage: bool
-
-        # only string and object types can have nones
-        case page.kind:
-        of K_UNICODE: canBeNone = true
-        of K_OBJECT:
-            let types = page.getPageTypes()
-
-            if K_NONETYPE in types:
-                if likely(types[K_NONETYPE] > 0):
-                    canBeNone = true
-
-            isObjectPage = true
-        else:
-            canBeNone = false
-            isObjectPage = false
-
+        # string arrays are first saved into separate sequences, after we collect them we will copy their memory to single buffer
         var longest = 1
-        let strings = collect:
-            for (i, v) in enumerate(page.pgIter):
-                var res: seq[Rune]
-
-                if unlikely(canBeNone):
-                    if not isObjectPage:
-                        when v is string:
-                            if v.len == 0:
-                                mask[i] = Mask.INVALID
-                                reason_lst[i] = createEmptyErrorReason()
-                                continue
-                    else:
-                        when v is PY_ObjectND:
-                            case v.kind:
-                            of K_STRING:
-                                if PY_String(v).value.len == 0:
-                                    mask[i] = Mask.INVALID
-                                    reason_lst[i] = createEmptyErrorReason()
-                                    continue
-                            of K_NONETYPE:
-                                mask[i] = Mask.INVALID
-                                reason_lst[i] = createNoneErrorReason()
-                                continue
-                            else:
-                                discard
-
-                try:
-                    let str = conv(v)
-                    res = str.toRunes
-
-                    longest = max(longest, res.len)
-                    mask[i] = Mask.VALID
-                except ValueError:
-                    mask[i] = Mask.INVALID
-
-                    var kind {.noinit.}: KindObjectND
-                    var strRepr {.noinit.}: string
-
-                    when v is PY_ObjectND:
-                        kind = v.kind
-                        strRepr = v.toRepr
-                    else:
-                        strRepr = $v
-                        implement("other")
-
-                    reason_lst[i] = createCastErrorReason(strRepr, kind)
-
-                    res = newSeq[Rune]()
-                    continue
-
-                res
-
-        let buf = newSeq[Rune](longest * page.len)
-        let shape = @[int(buf.len / longest)]
-
-        for (i, str) in enumerate(strings):
-            buf[i * longest].addr.copyMem(addr str[0], str.len * sizeof(Rune))
-
-        BaseNDArray T(shape: shape, buf: buf, size: longest, kind: K_UNICODE)
+        var runeBuf = newSeq[seq[Rune]](page.len)
     else:
-        let buf = collect:
-            for (i, v) in enumerate(page.pgIter):
-                var res = dt.default()
-                var skip = false
-                var alreadyChecked = false
+        # we know the maximum size of the sequence based on the page size as we cannot manifest new values
+        var buf = newSeqOfCap[T.baseType](page.len)
 
-                when v is PY_ObjectND:
+    when page is UnicodeNDArray or page is ObjectNDArray:
+        template addEmpty(i: int): void =
+            when T is UnicodeNDArray:
+                runeBuf.add(newSeq[Rune](0))
+            else:
+                when T is ObjectNDArray:
+                    buf.add(PY_None)
+            mask[i] = Mask.VALID
+
+    for (i, v) in enumerate(page.pgIter):
+        # 1. Emptiness test
+        when page is UnicodeNDArray:
+            # original page is made of strings, check for empty strings
+            if unlikely(v.len == 0):
+                # empty string
+                if not allow_empty:
+                    mask[i] = Mask.INVALID
+                    reason_lst[i] = createEmptyErrorReason()
+                else:
+                    addEmpty(i)
+                continue
+        else:
+            when page is ObjectNDArray:
+                # original page is made of python objects, check for empty strings and nones
+                if unlikely(canBeNone):
+                    # there can be empties in the page
                     case v.kind:
-                    of K_NONETYPE:
-                        if not allow_empty:
-                            reason_lst[i] = createNoneErrorReason()
-                            mask[i] = Mask.INVALID
-                            continue
-                        else:
-                            mask[i] = Mask.VALID
-                            alreadyChecked = true
                     of K_STRING:
                         if PY_String(v).value.len == 0:
+                            # empty string
                             if not allow_empty:
-                                reason_lst[i] = createNoneErrorReason()
                                 mask[i] = Mask.INVALID
-                                continue
+                                reason_lst[i] = createEmptyErrorReason()
                             else:
-                                mask[i] = Mask.VALID
-                                alreadyChecked = true
-                    else:
-                        discard
-                
-                if likely(not alreadyChecked):
-                    try:
-                        res = conv(v)
-                        mask[i] = Mask.VALID
-                    except ValueError:
-                        mask[i] = Mask.INVALID
-
-                        var kind {.noinit.}: KindObjectND
-                        var strRepr {.noinit.}: string
-
-                        when v is PY_ObjectND:
-                            kind = v.kind
-                            strRepr = v.toRepr
+                                addEmpty(i)
+                            continue
+                    of K_NONETYPE:
+                        if not allow_empty:
+                            # empty object
+                            mask[i] = Mask.INVALID
+                            reason_lst[i] = createNoneErrorReason()
                         else:
-                            strRepr = $v
-                            implement("other")
-
-                        reason_lst[i] = createCastErrorReason(strRepr, kind)
+                            addEmpty(i)
                         continue
+                    else:
+                        # other types are never empty
+                        discard
 
-                res
-        let shape = @[buf.len]
+        # 2. Attempt type conversion
+        try:
+            when T is UnicodeNDArray:
+                # our result is a string page, so we need to know the maximum length and convert to runes
+                let res = conv(v).toRunes
 
-        BaseNDArray T(shape: shape, buf: buf, kind: T.pageKind)
+                longest = max(longest, res.len)
+                runeBuf.add(res)
+            else:
+                buf.add(conv(v))
+
+            mask[i] = Mask.VALID
+        except ValueError:
+            # 2b. we couldn't cast the type, generate error string
+            when page is ObjectNDArray:
+                let inTypeKind = v.kind
+                let strRepr = v.toRepr
+            else:
+                let strRepr = $v
+
+            reason_lst[i] = createCastErrorReason(strRepr, inTypeKind)
+            mask[i] = Mask.INVALID
+
+    # 3. Dump the page
+    when T is UnicodeNDArray:
+        # we need to turn the individual strings into contigous block
+        let strCount = runeBuf.len
+        let buf = newSeq[Rune](longest * strCount)
+
+        for (i, str) in enumerate(runeBuf):
+            buf[i * longest].addr.copyMem(addr str[0], str.len * sizeof(Rune))
+
+        let res = T(shape: @[strCount], buf: buf, size: longest, kind: T.pageKind)
+    else:
+        let res = T(shape: @[buf.len], buf: buf, kind: T.pageKind)
+
+    BaseNDArray res
 
 type
     ToDate = object
@@ -241,8 +226,6 @@ proc newMkCaster(caster: NimNode, isPyCaster: bool): NimNode =
         trueCastR = newIdentNode(PY_Time.name)
     elif castR.strVal in [bool.name, int.name, float.name, string.name]:
         trueCastR = castR
-    # elif castR.strVal == PY_ObjectND.name:
-    #     trueCastR = castR
     else:
         raise newException(FieldDefect, "Uncastable return type '" & $castR.strVal & "'")
 
@@ -392,16 +375,15 @@ mkCasters:
         ToDateTime = infer.inferDatetime(addr v).value
         ToTime = infer.inferTime(addr v)
 
-# template obj2primCast[R](T1: typedesc, T2: typedesc, TR: typedesc[R], v: PY_ObjectND) = return T2.fnCast(TR)(T1(v).value)
 template obj2prim(v: PY_ObjectND) =
     case v.kind:
-    of K_BOOLEAN: bool.fnCast(R)(PY_Boolean(v).value) # PY_Boolean.obj2primCast(bool, R, v)
-    of K_INT: int.fnCast(R)(PY_Int(v).value) # PY_Int.obj2primCast(int, R, v)
-    of K_FLOAT: float.fnCast(R)(PY_Float(v).value) # PY_Float.obj2primCast(float, R, v)
-    of K_STRING: string.fnCast(R)(PY_String(v).value) # PY_String.obj2primCast(string, R, v)
-    of K_DATE: FromDate.fnCast(R)(PY_Date(v).value) # PY_Date.obj2primCast(FromDate, R, v)
+    of K_BOOLEAN: bool.fnCast(R)(PY_Boolean(v).value)
+    of K_INT: int.fnCast(R)(PY_Int(v).value)
+    of K_FLOAT: float.fnCast(R)(PY_Float(v).value)
+    of K_STRING: string.fnCast(R)(PY_String(v).value)
+    of K_DATE: FromDate.fnCast(R)(PY_Date(v).value)
     of K_TIME: PY_Time.fnCast(R)(PY_Time(v))
-    of K_DATETIME: FromDateTime.fnCast(R)(PY_Date(v).value) # PY_Date.obj2primCast(FromDateTime, R, v)
+    of K_DATETIME: FromDateTime.fnCast(R)(PY_Date(v).value)
     of K_NONETYPE: raise newException(ValueError, "cannot cast")
 
 mkCasters:
@@ -587,7 +569,7 @@ proc unusedMaskSearch(arr: var seq[Mask]): int =
 proc putPage(page: BaseNDArray, infos: var Table[string, nimpy.PyObject], colName: string, col: ColSliceInfo): void {.inline.} =
     let (dir, pid) = col
 
-    infos[colName] = newPyPage(pid, string dir, page.len, page.getPageTypes())
+    infos[colName] = newPyPage(pid, dir, page.len, page.getPageTypes())
 
 proc finalizeSlice(indices: var seq[int], column_names: seq[string], infos: var Table[string, nimpy.PyObject], cast_paths: var Table[string, (Path, Path, bool)], pages: var seq[(string, nimpy.PyObject)], result_info: ColInfo): void =
     if indices.len == 0:
@@ -835,7 +817,7 @@ proc collectColumnSelectInfo*(table: nimpy.PyObject, cols: nimpy.PyObject, dir_p
 
     var is_correct_type = initTable[string, bool]()
 
-    proc genpage(dirpid: string): ColSliceInfo {.inline.} = (dir_pid, tabliteBase().SimplePage.next_id(string dir_pid).to(int))
+    proc genpage(dirpid: string): ColSliceInfo {.inline.} = (dir_pid, tabliteBase().SimplePage.next_id(dir_pid).to(int))
 
     for (desired_name_non_unique, desired_columns) in desired_column_map.pairs():
         let keys = toSeq(passed_column_data)
